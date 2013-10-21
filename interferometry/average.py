c99e79117a565888da0df51f738ab9886fb44a2e
Patrick Sheehan <psheehan287@gmail.com>
2013-10-20 17:53:02 -0700

    Added the option to average the visibilities azimuthally.

--- interferometry/average.py
+++ interferometry/average.py
@@ -2,10 +2,11 @@ from numpy import where,zeros,sqrt,cos,sin,arange,array,abs,mat,ones,round, \
         linspace, meshgrid
 from .interferometry import Visibilities
 
-def average(data,gridsize=256,binsize=None):
+def average(data,gridsize=256,binsize=None,radial=False):
     
     u = data.u.copy()
     v = data.v.copy()
+    uvdist = data.uvdist.copy()
     real = data.real.copy()
     imag = data.imag.copy()
     weights = data.weights.copy()
@@ -17,16 +18,27 @@ def average(data,gridsize=256,binsize=None):
     
     # Average over the U-V plane by creating bins to average over.
     
-    uu = linspace(-(gridsize-1)*binsize/2,(gridsize-1)*binsize/2,gridsize)
-    vv = linspace(-(gridsize-1)*binsize/2,(gridsize-1)*binsize/2,gridsize)
+    if radial:
+        new_u = linspace(binsize/2,(gridsize-0.5)*binsize,gridsize)
+        new_u = new_u.reshape((1,gridsize))
+        new_v = zeros((1,gridsize))
+        new_real = zeros((1,gridsize))
+        new_imag = zeros((1,gridsize))
+        new_weights = zeros((1,gridsize))
 
-    new_u, new_v = meshgrid(uu, vv)
-    new_real = zeros((gridsize,gridsize))
-    new_imag = zeros((gridsize,gridsize))
-    new_weights = zeros((gridsize,gridsize))
+        i = round(uvdist/binsize)
+        j = zeros(uvdist.size)
+    else:
+        uu = linspace(-(gridsize-1)*binsize/2,(gridsize-1)*binsize/2,gridsize)
+        vv = linspace(-(gridsize-1)*binsize/2,(gridsize-1)*binsize/2,gridsize)
 
-    i = round(u/binsize+(gridsize-1)/2.)
-    j = round(v/binsize+(gridsize-1)/2.)
+        new_u, new_v = meshgrid(uu, vv)
+        new_real = zeros((gridsize,gridsize))
+        new_imag = zeros((gridsize,gridsize))
+        new_weights = zeros((gridsize,gridsize))
+
+        i = round(u/binsize+(gridsize-1)/2.)
+        j = round(v/binsize+(gridsize-1)/2.)
     
     for k in range(u.size):
         new_real[j[k],i[k]] += (real[k,:]*weights[k,:]).sum()
