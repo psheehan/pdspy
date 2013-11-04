8ac77542c6a5a54a96de134a1417e4e72806d886
Patrick Sheehan <psheehan287@gmail.com>
2013-11-03 20:41:54 -0700

    Updated imtovis to deal with spectral images.

--- imaging/imtovis.py
+++ imaging/imtovis.py
@@ -3,30 +3,31 @@ from ..interferometry.interferometry import Visibilities
 from ..constants.astronomy import pc, arcsec
 from scipy.fftpack import fft2,fftshift
 
-def imtovis(image,dpc=140,ext=0):
+def imtovis(image, dpc=140):
 
     ##### Some natural constants
-    
-    im = image.image[:,:,ext]
     
     if dpc == None:
         r = 140*pc
     else:
         r = dpc*pc
     
-    vis = fftshift(fft2(fftshift(im)))
+    real = numpy.empty((image.x.size*image.y.size,image.freq.size))
+    imag = numpy.empty((image.x.size*image.y.size,image.freq.size))
+    weights = numpy.ones(real.shape)
+    for i in range(image.freq.size):
+        vis = fftshift(fft2(fftshift(image.image[:,:,i])))
+        real[:,i] = vis.real.reshape((image.x.size*image.y.size,))
+        imag[:,i] = vis.imag.reshape((image.x.size*image.y.size,))
     
     max_x = 1.0 / ( (image.x[1] - image.x[0])/r *arcsec )
     max_y = 1.0 / ( (image.y[1] - image.y[0])/r *arcsec )
     
     u, v = numpy.meshgrid( numpy.linspace(-max_x, max_x, image.x.size), \
             numpy.linspace(-max_y, max_y, image.y.size), indexing='ij')
-    u = u.reshape((image.x.size**2,))
-    v = v.reshape((image.y.size**2,))
+    u = u.reshape((image.x.size*image.y.size,))
+    v = v.reshape((image.y.size*image.y.size,))
 
-    freq = numpy.array([image.freq[ext]])
-    real = (vis.real).reshape((image.x.size**2,1))
-    imag = (vis.imag).reshape((image.x.size**2,1))
-    weights = numpy.ones(real.shape)
-    
+    freq = image.freq
+
     return Visibilities(u,v,freq,real,imag,weights)
