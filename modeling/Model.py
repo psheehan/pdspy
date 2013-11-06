a50899fbe1efd7b7d353a1095000d8c275285bd6
Patrick Sheehan <psheehan287@gmail.com>
2013-11-06 14:56:37 -0700

    Added some parameters for imaging to Model

--- modeling/Model.py
+++ modeling/Model.py
@@ -160,7 +160,7 @@ class Model:
 
     def run_image_radmc3d(self, name=None, nphot=1e6, npix=256, sizeau=1000, \
             lam="1300", iline=None,  widthkms=None, linenlam=None, \
-            incl=0, **keywords):
+            incl=0, pa=0, phi=0, **keywords):
         radmc3d.write.control(nphot_scat=nphot, **keywords)
 
         mstar = []
@@ -220,7 +220,8 @@ class Model:
         radmc3d.write.gas_velocity(self.grid.velocity[0])
 
         radmc3d.run.image(npix=npix, sizeau=sizeau, lam=lam, iline=iline, \
-                widthkms=widthkms, linenlam=linenlam, incl=incl)
+                widthkms=widthkms, linenlam=linenlam, incl=incl, posang=pa, \
+                phi=phi)
 
         image, x, y, lam = radmc3d.read.image()
 
