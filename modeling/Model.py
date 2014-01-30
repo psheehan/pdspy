47e31ea19286ffce86c9d962ec36d03df9919de7
Patrick Sheehan <psheehan287@gmail.com>
2014-01-29 20:57:05 -0700

    Model can now run spectra with RADMC-3D.

--- modeling/Model.py
+++ modeling/Model.py
@@ -139,6 +139,29 @@ class Model:
 
         os.system("rm *.out *.inp *.dat")
 
+    def run_spectrum(self, name=None, nphot=1e6, code="radmc3d", **keywords):
+        if (code == "radmc3d"):
+            self.run_spectrum_radmc3d(name=name, nphot=nphot, **keywords)
+        else:
+            self.run_spectrum_hyperion(name=name, nphot=nphot, **keywords)
+
+    def run_spectrum_hyperion(self, name=None, nphot=1e6):
+        return
+
+    def run_spectrum_radmc3d(self, name=None, nphot=1e6, incl=0, pa=0, \
+            phi=0, dpc=1, **keywords):
+        self.write_radmc3d(nphot_spec=nphot, **keywords)
+
+        radmc3d.run.sed(incl=incl, posang=pa, phi=phi)
+
+        flux, lam = radmc3d.read.spectrum()
+
+        flux = flux / Jy * (1. / dpc)**2
+
+        self.spectra[name] = Spectrum(wave=lam, flux=flux)
+
+        os.system("rm *.out *.inp *.dat")
+
     def run_visibilities(self, name=None, nphot=1e6, code="radmc3d", \
             **keywords):
         if (code == "radmc3d"):
