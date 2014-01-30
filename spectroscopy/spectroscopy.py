2f7c99e08da65f76578591cd55f06a9be5e09d7b
Patrick Sheehan <psheehan287@gmail.com>
2014-01-29 21:06:07 -0700

    Added read/write to hdf5 files to Spectrum.

--- spectroscopy/spectroscopy.py
+++ spectroscopy/spectroscopy.py
@@ -2,22 +2,27 @@ import ctypes
 import numpy
 import os
 import astropy
+import h5py
 
 lib = ctypes.cdll.LoadLibrary(os.path.dirname(__file__)+'/libspectroscopy.so')
 lib.new_Spectrum.restype = ctypes.c_void_p
 
 class Spectrum:
 
-    def __init__(self, wave, flux, unc):
-        self.wave = wave
-        self.flux = flux
-        self.unc = unc
+    def __init__(self, wave=None, flux=None, unc=None):
+        if (wave != None):
+            self.wave = wave
+            self.flux = flux
+            if unc == None:
+                self.unc = numpy.zeros(wave.size)
+            else:
+                self.unc = unc
 
-        self.obj = lib.new_Spectrum( \
-                wave.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), \
-                flux.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), \
-                unc.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), \
-                ctypes.c_int(wave.size))
+            self.obj = lib.new_Spectrum( \
+                    wave.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), \
+                    flux.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), \
+                    unc.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), \
+                    ctypes.c_int(wave.size))
 
     def __del__(self):
         lib.delete_Spectrum(ctypes.c_void_p(self.obj))
@@ -32,3 +37,35 @@ class Spectrum:
         hdulist.append(hdu)
 
         return hdulist
+
+    def read(self, filename=None, usefile=None):
+        if (usefile == None):
+            f = h5py.File(filename, "r")
+        else:
+            r = usefile
+
+        wave = f['wave'].value
+        flux = f['flux'].value
+        unc = f['unc'].value
+
+        self.__init__(wave, flux, unc)
+
+        if (usefile == None):
+            f.close()
+
+    def write(self, filename=None, usefile=None):
+        if (usefile == None):
+            f = h5py.File(filename, "w")
+        else:
+            f = usefile
+
+        if hasattr(self, "wave"):
+            wave_dset = f.create_dataset("wave", (self.wave.size,), dtype='f')
+            wave_dset[...] = self.wave
+            flux_dset = f.create_dataset("flux", (self.flux.size,), dtype='f')
+            flux_dset[...] = self.flux
+            unc_dset = f.create_dataset("unc", (self.unc.size,), dtype='f')
+            unc_dset[...] = self.unc
+
+        if (usefile == None):
+            f.close()
