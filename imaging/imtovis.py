fafb766671f8e679ef6634a97e149c12a4852878
Patrick Sheehan <psheehan287@gmail.com>
2014-01-30 21:07:06 -0700

    Fixed imtovis so that it calculates the visibilities correctly regardless of whether the image has an even or odd number of pixels (per axis)

--- imaging/imtovis.py
+++ imaging/imtovis.py
@@ -1,7 +1,7 @@
 import numpy
 from ..interferometry.interferometry import Visibilities
 from ..constants.astronomy import pc, arcsec
-from scipy.fftpack import fft2, fftshift, fftfreq
+from scipy.fftpack import fft2, fftshift, fftfreq, ifftshift
 
 def imtovis(image):
 
@@ -11,10 +11,10 @@ def imtovis(image):
     imag = numpy.empty((image.x.size*image.y.size,image.freq.size))
     weights = numpy.ones(real.shape)
     for i in range(image.freq.size):
-        vis = fftshift(fft2(fftshift(image.image[:,:,i])))
+        vis = fftshift(fft2(ifftshift(image.image[:,:,i])))
         real[:,i] = vis.real.reshape((image.x.size*image.y.size,))
         imag[:,i] = vis.imag.reshape((image.x.size*image.y.size,))
-    
+
     uu = fftshift(fftfreq(image.x.size, (image.x[1] - image.x[0]) * arcsec))
     vv = fftshift(fftfreq(image.y.size, (image.y[1] - image.y[0]) * arcsec))
     
