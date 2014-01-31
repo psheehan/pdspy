30fb4c01c3e331c6dbff7dd5aa2b04ba455011b7
Patrick Sheehan <psheehan287@gmail.com>
2014-01-30 17:46:04 -0700

    Updated imtovis to get the correct spatial frequencies given the pixel size of the image.

--- imaging/imtovis.py
+++ imaging/imtovis.py
@@ -1,7 +1,7 @@
 import numpy
 from ..interferometry.interferometry import Visibilities
 from ..constants.astronomy import pc, arcsec
-from scipy.fftpack import fft2,fftshift
+from scipy.fftpack import fft2, fftshift, fftfreq
 
 def imtovis(image):
 
@@ -15,11 +15,10 @@ def imtovis(image):
         real[:,i] = vis.real.reshape((image.x.size*image.y.size,))
         imag[:,i] = vis.imag.reshape((image.x.size*image.y.size,))
     
-    max_x = 1.0 / ( (image.x[1] - image.x[0]) * arcsec )
-    max_y = 1.0 / ( (image.y[1] - image.y[0]) * arcsec )
+    uu = fftshift(fftfreq(image.x.size, (image.x[1] - image.x[0]) * arcsec))
+    vv = fftshift(fftfreq(image.y.size, (image.y[1] - image.y[0]) * arcsec))
     
-    u, v = numpy.meshgrid( numpy.linspace(-max_x, max_x, image.x.size), \
-            numpy.linspace(-max_y, max_y, image.y.size), indexing='ij')
+    u, v = numpy.meshgrid(uu, vv, indexing='ij')
     u = u.reshape((image.x.size*image.y.size,))
     v = v.reshape((image.y.size*image.y.size,))
 
