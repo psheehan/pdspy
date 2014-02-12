3373e412c73c959900d1829501e555271d9f5444
Patrick Sheehan <psheehan287@gmail.com>
2014-02-11 22:55:27 -0700

    Updated average to 2D average in a better way.

--- interferometry/average.py
+++ interferometry/average.py
@@ -1,5 +1,4 @@
-from numpy import where,zeros,sqrt,cos,sin,arange,array,abs,mat,ones,round, \
-        linspace, meshgrid
+import numpy
 from .interferometry import Visibilities
 
 def average(data,gridsize=256,binsize=None,radial=False):
@@ -12,33 +11,45 @@ def average(data,gridsize=256,binsize=None,radial=False):
     weights = data.weights.copy()
     
     # Set the weights equal to 0 when the point is flagged (i.e. weight < 0)
-    weights = where(weights < 0,0.0,weights)
+    weights = numpy.where(weights < 0,0.0,weights)
     # Set the weights equal to 0 when the real and imaginary parts are both 0
     weights[(real == 0) & (imag == 0)] = 0.0
     
     # Average over the U-V plane by creating bins to average over.
     
     if radial:
-        new_u = linspace(binsize/2,(gridsize-0.5)*binsize,gridsize)
+        new_u = numpy.linspace(binsize/2,(gridsize-0.5)*binsize,gridsize)
         new_u = new_u.reshape((1,gridsize))
-        new_v = zeros((1,gridsize))
-        new_real = zeros((1,gridsize))
-        new_imag = zeros((1,gridsize))
-        new_weights = zeros((1,gridsize))
+        new_v = numpy.zeros((1,gridsize))
+        new_real = numpy.zeros((1,gridsize))
+        new_imag = numpy.zeros((1,gridsize))
+        new_weights = numpy.zeros((1,gridsize))
 
-        i = round(uvdist/binsize)
-        j = zeros(uvdist.size)
+        i = numpy.round(uvdist/binsize)
+        j = numpy.zeros(uvdist.size)
     else:
-        uu = linspace(-(gridsize-1)*binsize/2,(gridsize-1)*binsize/2,gridsize)
-        vv = linspace(-(gridsize-1)*binsize/2,(gridsize-1)*binsize/2,gridsize)
+        if gridsize%2 == 0:
+            uu = numpy.linspace(-gridsize*binsize/2, (gridsize/2-1)*binsize, \
+                    gridsize)
+            vv = numpy.linspace(-gridsize*binsize/2, (gridsize/2-1)*binsize, \
+                    gridsize)
+        else:
+            uu = numpy.linspace(-(gridsize-1)*binsize/2, \
+                    (gridsize-1)*binsize/2, gridsize)
+            vv = numpy.linspace(-(gridsize-1)*binsize/2, \
+                    (gridsize-1)*binsize/2, gridsize)
 
-        new_u, new_v = meshgrid(uu, vv)
-        new_real = zeros((gridsize,gridsize))
-        new_imag = zeros((gridsize,gridsize))
-        new_weights = zeros((gridsize,gridsize))
+        new_u, new_v = numpy.meshgrid(uu, vv)
+        new_real = numpy.zeros((gridsize,gridsize))
+        new_imag = numpy.zeros((gridsize,gridsize))
+        new_weights = numpy.zeros((gridsize,gridsize))
 
-        i = round(u/binsize+(gridsize-1)/2.)
-        j = round(v/binsize+(gridsize-1)/2.)
+        if gridsize%2 == 0:
+            i = numpy.round(u/binsize+gridsize/2.)
+            j = numpy.round(v/binsize+gridsize/2.)
+        else:
+            i = round(u/binsize+(gridsize-1)/2.)
+            j = round(v/binsize+(gridsize-1)/2.)
     
     for k in range(u.size):
         new_real[j[k],i[k]] += (real[k,:]*weights[k,:]).sum()
@@ -54,6 +65,6 @@ def average(data,gridsize=256,binsize=None,radial=False):
             (new_u.size,1))
     new_weights = new_weights[good_data].reshape((new_u.size,1))
     
-    freq = array([data.freq.sum()/data.freq.size])
+    freq = numpy.array([data.freq.sum()/data.freq.size])
     
     return Visibilities(new_u,new_v,freq,new_real,new_imag,new_weights)
