f3c151268b9e4310b17fbef3bbec1c2db5d1bb12
Patrick Sheehan <psheehan287@gmail.com>
2014-02-12 09:34:38 -0700

    Updated grid to grid the data the way that an FFT wants the data to be gridded.

--- interferometry/grid.py
+++ interferometry/grid.py
@@ -2,32 +2,31 @@ from numpy import where,zeros,sqrt,cos,sin,arange,array,exp,abs,mat,ones, \
                   round,sinc
 from .interferometry import Visibilities
 from .freqcorrect import freqcorrect
-from ..constants.math import pi
 
-def grid(data,gridsize=256,binsize=2.0,channels=False,convolution="pillbox", \
-           mfs=False,channel=None):
+def grid(data, gridsize=256, binsize=2000.0, channels=False, \
+        convolution="pillbox", mfs=False,channel=None):
     
     if mfs:
         vis = freqcorrect(data)
-        u = vis.u
-        v = vis.v
-        real = vis.real
-        imag = vis.imag
-        weights = vis.weights
+        u = vis.u.copy()
+        v = vis.v.copy()
+        real = vis.real.copy()
+        imag = vis.imag.copy()
+        weights = vis.weights.copy()
     else:
-        u = data.u
-        v = data.v
+        u = data.u.copy
+        v = data.v.copy
         if channel != None:
             real = data.real[:,channel].reshape((data.real.shape[0],1))
             imag = data.imag[:,channel].reshape((data.real.shape[0],1))
             weights = data.weights[:,channel].reshape((data.real.shape[0],1))
         else:
-            real = data.real
-            imag = data.imag
-            weights = data.weights
+            real = data.real.copy()
+            imag = data.imag.copy()
+            weights = data.weights.copy()
     
     # Set the weights equal to 0 when the point is flagged (i.e. weight < 0)
-    weights = where(weights < 0,0.0,weights)
+    weights = numpy.where(weights < 0,0.0,weights)
     # Set the weights equal to 0 when the real and imaginary parts are both 0
     weights[(real==0) & (imag==0)] = 0.0
     
@@ -40,14 +39,28 @@ def grid(data,gridsize=256,binsize=2.0,channels=False,convolution="pillbox", \
     else:
         nchannels = real[0,:].size
     
-    new_u = array(mat(ones(gridsize)).T*(arange(gridsize)-gridsize/2.)*binsize)
-    new_v = array(mat(arange(gridsize)-gridsize/2.).T*binsize*ones(gridsize))
-    new_real = zeros((gridsize,gridsize))
-    new_imag = zeros((gridsize,gridsize))
-    new_weights = zeros((gridsize,gridsize))
+    if gridsize%2 == 0:
+        uu = numpy.linspace(-gridsize*binsize/2, (gridsize/2-1)*binsize, \
+                gridsize)
+        vv = numpy.linspace(-gridsize*binsize/2, (gridsize/2-1)*binsize, \
+                gridsize)
+    else:
+        uu = numpy.linspace(-(gridsize-1)*binsize/2, (gridsize-1)*binsize/2, \
+                gridsize)
+        vv = numpy.linspace(-(gridsize-1)*binsize/2, (gridsize-1)*binsize/2, \
+                gridsize)
 
-    i = round(u/binsize+gridsize/2.)
-    j = round(v/binsize+gridsize/2.)
+    new_u, new_v = numpy.meshgrid(uu, vv)
+    new_real = numpy.zeros((gridsize,gridsize))
+    new_imag = numpy.zeros((gridsize,gridsize))
+    new_weights = numpy.zeros((gridsize,gridsize))
+
+    if gridsize%2 == 0:
+        i = numpy.round(u/binsize+gridsize/2.)
+        j = numpy.round(v/binsize+gridsize/2.)
+    else:
+        i = numpy.round(u/binsize+(gridsize-1)/2.)
+        j = numpy.round(v/binsize+(gridsize-1)/2.)
     
     if convolution == "pillbox":
         convolve_func = ones_arr
@@ -56,7 +69,7 @@ def grid(data,gridsize=256,binsize=2.0,channels=False,convolution="pillbox", \
         convolve_func = exp_sinc
         ninclude = 7
     
-    inc_range = arange(ninclude)-(ninclude-1)/2
+    inc_range = numpy.linspace(-(ninclude-1)/2, (ninclude-1)/2, ninclude)
     for k in range(u.size):
         for l in inc_range+j[k]:
             for m in inc_range+i[k]:
