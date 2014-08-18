2dc00a3145611b663978e4eee537d5cda4aa85cb
Patrick Sheehan <psheehan287@gmail.com>
2014-08-18 16:35:15 -0700

    Added the grid function to libinterferometry. Needs to be tested before making it THE grid function for the interferometry package.

--- pdspy/interferometry/libinterferometry.pyx
+++ pdspy/interferometry/libinterferometry.pyx
@@ -2,6 +2,7 @@ import numpy
 cimport numpy
 import h5py
 import astropy
+#from .freqcorrect import freqcorrect
 
 cdef class VisibilitiesObject:
     cdef public numpy.ndarray u, v, freq, real, imag, weights, \
@@ -163,8 +164,8 @@ def average(data, gridsize=256, binsize=None, radial=False):
 
     for k in range(nuv):
         for l in range(nfreq):
-            new_real[j[k],i[k]] += (real[k,l]*weights[k,l])
-            new_imag[j[k],i[k]] += (imag[k,l]*weights[k,l])
+            new_real[j[k],i[k]] += real[k,l]*weights[k,l]
+            new_imag[j[k],i[k]] += imag[k,l]*weights[k,l]
             new_weights[j[k],i[k]] += weights[k,l]
     
     good_data = new_weights != 0.0
@@ -179,3 +180,125 @@ def average(data, gridsize=256, binsize=None, radial=False):
     freq = numpy.array([data.freq.sum()/data.freq.size])
     
     return Visibilities(new_u,new_v,freq,new_real,new_imag,new_weights)
+
+def grid(data, gridsize=256, binsize=2000.0, convolution="pillbox", \
+        mfs=False, channel=None, imaging=False):
+    
+    cdef numpy.ndarray[double, ndim=1] u, v, freq
+    cdef numpy.ndarray[double, ndim=2] real, imag, weights, new_u, new_v, \
+            new_real, new_imag, new_weights
+    cdef numpy.ndarray[unsigned int, ndim=1] i, j
+    cdef unsigned int k, l, m, n
+
+    if mfs:
+        vis = data#vis = freqcorrect(data)
+        u = vis.u.copy()
+        v = vis.v.copy()
+        freq = vis.freq.copy()
+        real = vis.real.copy()
+        imag = vis.imag.copy()
+        weights = vis.weights.copy()
+    else:
+        u = data.u.copy()
+        v = data.v.copy()
+        if channel != None:
+            freq = numpy.array([data.freq[channel]])
+            real = data.real[:,channel].copy().reshape((data.real.shape[0],1))
+            imag = data.imag[:,channel].copy().reshape((data.real.shape[0],1))
+            weights = data.weights[:,channel].copy(). \
+                    reshape((data.real.shape[0],1))
+        else:
+            freq = numpy.array([data.freq.mean()])
+            real = data.real.copy()
+            imag = data.imag.copy()
+            weights = data.weights.copy()
+    
+    # Set the weights equal to 0 when the point is flagged (i.e. weight < 0)
+    weights = numpy.where(weights < 0, 0.0, weights)
+    # Set the weights equal to 0 when the real and imaginary parts are both 0
+    weights[(real==0) & (imag==0)] = 0.0
+    
+    if imaging:
+        weights /= weights.sum()
+    
+    # Average over the U-V plane by creating bins to average over.
+    
+    if gridsize%2 == 0:
+        uu = numpy.linspace(-gridsize*binsize/2, (gridsize/2-1)*binsize, \
+                gridsize)
+        vv = numpy.linspace(-gridsize*binsize/2, (gridsize/2-1)*binsize, \
+                gridsize)
+    else:
+        uu = numpy.linspace(-(gridsize-1)*binsize/2, (gridsize-1)*binsize/2, \
+                gridsize)
+        vv = numpy.linspace(-(gridsize-1)*binsize/2, (gridsize-1)*binsize/2, \
+                gridsize)
+
+    new_u, new_v = numpy.meshgrid(uu, vv)
+    new_real = numpy.zeros((gridsize,gridsize))
+    new_imag = numpy.zeros((gridsize,gridsize))
+    new_weights = numpy.zeros((gridsize,gridsize))
+
+    if gridsize%2 == 0:
+        i = numpy.round(u/binsize+gridsize/2.).astype(numpy.uint32)
+        j = numpy.round(v/binsize+gridsize/2.).astype(numpy.uint32)
+    else:
+        i = numpy.round(u/binsize+(gridsize-1)/2.).astype(numpy.uint32)
+        j = numpy.round(v/binsize+(gridsize-1)/2.).astype(numpy.uint32)
+    
+    if convolution == "pillbox":
+        convolve_func = ones
+        ninclude = 3
+    elif convolution == "expsinc":
+        convolve_func = exp_sinc
+        ninclude = 9
+
+    cdef int nuv = u.size
+    cdef int nfreq = freq.size
+    cdef double convolve
+    cdef numpy.ndarray[int, ndim=1] inc_range = numpy.linspace(-(ninclude-1)/2,\
+            (ninclude-1)/2, ninclude).astype(numpy.int32)
+
+    for k in range(nuv):
+        for l in inc_range+j[k]:
+            for m in inc_range+i[k]:
+                convolve = convolve_func(u[k]-new_u[l,m], \
+                        v[k] - new_v[l,m], binsize, binsize)
+                for n in range(nfreq):
+                    new_real[l,m] += real[k,n]*weights[k,n]*convolve
+                    new_imag[l,m] += imag[k,n]*weights[k,n]*convolve
+                    new_weights[l,m] += weights[k,n]*convolve
+    
+    new_real = new_real.reshape((gridsize**2,1))
+    new_imag = new_imag.reshape((gridsize**2,1))
+    new_weights = new_weights.reshape((gridsize**2,1))
+
+    return Visibilities(new_u.reshape(gridsize**2), new_v.reshape(gridsize**2),\
+            freq, new_real, new_imag, new_weights)
+
+cdef double exp_sinc(double u, double v, double delta_u, double delta_v):
+    
+    cdef double alpha1 = 1.55
+    cdef double alpha2 = 2.52
+    cdef int m = 6
+    
+    cdef double arr = numpy.sinc(u / (alpha1 * delta_u)) * \
+            numpy.exp(-1 * (u / (alpha2 * delta_u))**2)* \
+            numpy.sinc(v / (alpha1 * delta_v))* \
+            numpy.exp(-1 * (v / (alpha2 * delta_v))**2)
+
+    if (abs(u) >= m * delta_u / 2) or (abs(v) >= m * delta_v / 2):
+        arr = 0.
+
+    return arr
+
+cdef double ones(double u, double v, double delta_u, double delta_v):
+    
+    cdef int m = 1
+
+    cdef double arr = 1.0
+
+    if (abs(u) >= m * delta_u / 2) or (abs(v) >= m * delta_v / 2):
+        arr = 0.
+
+    return arr
