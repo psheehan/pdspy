a39f81c25205a1524b75d6eedf15f95ecc8e2de6
Patrick Sheehan <psheehan287@gmail.com>
2014-08-19 10:40:57 -0700

    Added freqcorrect to libinterferometry because grid depends on freqcorrect and freqcorrect depends on Visibilities. It became a recursive problem to have freqcorrect in a separate place.

--- pdspy/interferometry/__init__.py
+++ pdspy/interferometry/__init__.py
@@ -1,11 +1,10 @@
-from .libinterferometry import Visibilities, average, grid
+from .libinterferometry import Visibilities, average, grid, freqcorrect
 from .readuvfits import readuvfits
 from .readvis import readvis
 from .center import center
 from .clean import clean
 from .concatenate import concatenate
 from .fit_model import fit_model
-from .freqcorrect import freqcorrect
 from .invert import invert
 from .model import model
 from .rotate import rotate
--- pdspy/interferometry/libinterferometry.pyx
+++ pdspy/interferometry/libinterferometry.pyx
@@ -2,7 +2,6 @@ import numpy
 cimport numpy
 import h5py
 import astropy
-#from .freqcorrect import freqcorrect
 
 cdef class VisibilitiesObject:
     cdef public numpy.ndarray u, v, freq, real, imag, weights, \
@@ -192,7 +191,7 @@ def grid(data, gridsize=256, binsize=2000.0, convolution="pillbox", \
             mmin, mmax
 
     if mfs:
-        vis = data#vis = freqcorrect(data)
+        vis = freqcorrect(data)
         u = vis.u.copy()
         v = vis.v.copy()
         freq = vis.freq.copy()
@@ -308,3 +307,43 @@ cdef double ones(double u, double v, double delta_u, double delta_v):
         arr = 0.
 
     return arr
+
+def freqcorrect(data, new_freq=None):
+
+    if new_freq != None:
+        new_freq = numpy.array([new_freq])
+    else:
+        new_freq = numpy.array([data.freq.mean()])
+
+    new_u = numpy.array([])
+    new_v = numpy.array([])
+    new_real = numpy.array([])
+    new_imag = numpy.array([])
+    new_weights = numpy.array([])
+    new_baseline = numpy.array([])
+
+    for i in range(data.freq.size):
+        new_u = numpy.concatenate((new_u, data.u * data.freq[i]/new_freq[0]))
+        new_v = numpy.concatenate((new_v, data.v * data.freq[i]/new_freq[0]))
+        new_real = numpy.concatenate((new_real, data.real[:,i].copy()))
+        new_imag = numpy.concatenate((new_imag, data.imag[:,i].copy()))
+        new_weights = numpy.concatenate((new_weights, data.weights[:,i].copy()))
+        new_baseline = numpy.concatenate((new_baseline, data.baseline.copy()))
+
+    new_u = new_u.reshape((new_u.size,))
+    new_v = new_v.reshape((new_v.size,))
+    new_real = new_real.reshape((new_real.size,1))
+    new_imag = new_imag.reshape((new_imag.size,1))
+    new_weights = new_weights.reshape((new_weights.size,1))
+    new_baseline = new_baseline.reshape((new_baseline.size,))
+
+    good = new_weights[:,0] > 0.0
+    new_u = new_u[good]
+    new_v = new_v[good]
+    new_real = new_real[good,:]
+    new_imag = new_imag[good,:]
+    new_weights = new_weights[good,:]
+    new_baseline = new_baseline[good]
+
+    return Visibilities(new_u, new_v, new_freq, new_real, new_imag, \
+            new_weights, baseline=new_baseline)
