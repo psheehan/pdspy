8690e0264edbbdc3528f52c69cf5b06d3a97701e
Patrick Sheehan <psheehan287@gmail.com>
2015-01-15 13:20:57 -0700

    match_source_lists can now match >2 lists together.

--- pdspy/imaging/match_source_lists.py
+++ pdspy/imaging/match_source_lists.py
@@ -2,30 +2,67 @@ import numpy
 import astropy
 import astropy.coordinates
 
-def match_source_lists(list1, list2, tol=0.3, table_names=['1', '2']):
+def match_source_lists(*lists, tol=0.3, table_names=['1', '2']):
 
-    coord1 = astropy.coordinates.SkyCoord(list1["ra"].tolist(), \
-            list1['dec'].tolist(), 'icrs')
-    coord2 = astropy.coordinates.SkyCoord(list2["ra"].tolist(), \
-            list2["dec"].tolist(), 'icrs')
+    for i in range(len(lists)-1):
+        if i == 0:
+            list1 = lists[i]
+        else:
+            list1 = joined_list
+        list2 = lists[i+1]
 
-    idx, d2d, d3d = astropy.coordinates.match_coordinates_sky(coord1,coord2)
+        coord1 = astropy.coordinates.SkyCoord(list1["ra"].tolist(), \
+                list1['dec'].tolist(), 'icrs')
+        coord2 = astropy.coordinates.SkyCoord(list2["ra"].tolist(), \
+                list2["dec"].tolist(), 'icrs')
 
-    dra = (coord1.ra - coord2[idx].ra).arcsec
-    ddec = (coord1.ra - coord2[idx].ra).arcsec
+        idx, d2d, d3d = astropy.coordinates.match_coordinates_sky(coord1,coord2)
 
-    d = (dra**2 + ddec**2)**0.5
+        dra = (coord1.ra - coord2[idx].ra).arcsec
+        ddec = (coord1.ra - coord2[idx].ra).arcsec
 
-    list1["id"] = numpy.arange(len(list1)) + len(list2)
-    list2["id"] = numpy.arange(len(list2))
-    list1["id"][d < tol] = idx[d < tol]
-    
-    joined_list = astropy.table.join(list1, list2, join_type='outer', \
-            keys='id', table_names=table_names, \
-            uniq_col_name='{col_name}_{table_name}')
+        d = (dra**2 + ddec**2)**0.5
 
-    del list1["id"]
-    del list2["id"]
-    del joined_list["id"]
+        list1["id"] = numpy.arange(len(list1)) + len(list2)
+        list2["id"] = numpy.arange(len(list2))
+        list1["id"][d < tol] = idx[d < tol]
+
+        if i == 0:
+            names = table_names[0:2]
+        else:
+            names = ["",table_names[i+1]]
+        
+        joined_list = astropy.table.join(list1, list2, join_type='outer', \
+                keys='id', table_names=names, \
+                uniq_col_name='{col_name}_{table_name}')
+
+        del list1["id"]
+        del list2["id"]
+        del joined_list["id"]
+
+        if i == 0:
+            joined_list["ra"] = numpy.where(\
+                    joined_list["ra_"+names[0]].mask, \
+                    joined_list["ra_"+names[1]], joined_list["ra_"+names[0]])
+            joined_list["dec"] = numpy.where(\
+                    joined_list["dec_"+names[0]].mask, \
+                    joined_list["dec_"+names[1]], joined_list["dec_"+names[0]])
+        else:
+            for name in list2.colnames:
+                if name in joined_list.colnames:
+                    joined_list.rename_column(name, name+"_"+names[1])
+
+            joined_list.rename_column("ra_","ra")
+            joined_list.rename_column("dec_","dec")
+
+            joined_list["ra"] = numpy.where(\
+                    joined_list["ra"].mask, joined_list["ra_"+names[1]], \
+                    joined_list["ra"])
+            joined_list["dec"] = numpy.where(\
+                    joined_list["dec"].mask, joined_list["dec_"+names[1]], \
+                    joined_list["dec"])
+
+    del joined_list["ra"]
+    del joined_list["dec"]
 
     return joined_list
