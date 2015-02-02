604ff28b04d4bd2c70a157f7257b7b494dd423a0
Patrick Sheehan <psheehan287@gmail.com>
2015-02-02 12:29:00 -0700

    Added a routine to take an existing catalog and add data to it for sources in the catalog that are missing certain bands of data.

--- pdspy/imaging/__init__.py
+++ pdspy/imaging/__init__.py
@@ -3,3 +3,4 @@ from .imtovis import imtovis
 from .readimfits import readimfits
 from .find import find
 from .match_source_lists import match_source_lists
+from .update_catalog import update_catalog
---
+++ pdspy/imaging/update_catalog.py
@@ -0,0 +1,33 @@
+import numpy
+import astropy
+import astropy.coordinates
+
+def update_catalog(catalog, *lists, tol=0.3, column_names=['1']):
+
+    coord_catalog = astropy.coordinates.SkyCoord(catalog["ra"].tolist(), \
+            catalog["dec"].tolist(), 'icrs')
+    catalog["id"] = numpy.arange(len(catalog))
+
+    for i, list1 in enumerate(lists):
+        coord1 = astropy.coordinates.SkyCoord(list1["ra"].tolist(), \
+                list1['dec'].tolist(), 'icrs')
+
+        idx, d2d, d3d = astropy.coordinates.match_coordinates_sky(coord1, \
+                coord_catalog)
+
+        ids, counts = numpy.unique(idx[d2d.arcsec < tol], return_counts=True)
+        for j in range(len(ids)):
+            if counts[j] > 1:
+                min_d2d = min(d2d[idx == ids[j]].arcsec)
+                d2d[(d2d.arcsec != min_d2d) & (idx == ids[j])] = \
+                        astropy.coordinates.Angle("0d00m{0:f}s".format(2*tol))
+
+        for j, k in enumerate(idx):
+            if d2d.arcsec[j] < tol:
+                for key in list1.colnames:
+                    catalog[key+"_{0}".format(column_names[i])][k] = \
+                            list1[key][j]
+
+    del catalog["id"]
+
+    return catalog
