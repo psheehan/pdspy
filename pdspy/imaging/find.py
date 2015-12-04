060f910a1aac3e29ef7f4628e19ef53cb2c7b0e3
Patrick Sheehan <psheehan287@gmail.com>
2015-12-04 16:19:54 -0700

    Updated find to use bootstrapping (with a good interferometer noise pattern) to calculate the uncertainties on the fits in a more realistic way.

--- pdspy/imaging/find.py
+++ pdspy/imaging/find.py
@@ -178,6 +178,38 @@ def find(image, threshold=5, include_radius=20, window_size=40, \
             p, cov, infodict, mesg, ier = scipy.optimize.leastsq(func, \
                     params, args=(1, x, y, z2, sigma_z2), full_output=True)
 
+        # Now calculate the uncertainty on the parameters using bootstrapping.
+
+        s_res = sigma_z.mean()
+        ps = []
+        bm = [image.header["BMAJ"]/abs(image.wcs.wcs.cdelt[0])/2.355,\
+                image.header["BMIN"]/abs(image.wcs.wcs.cdelt[0])/2.355,\
+                image.header["BPA"]*numpy.pi/180.]
+
+        for i in range(100):
+            randomDelta = interferometer_noise(z.shape[0], s_res, bm)
+            randomdataZ = z + randomDelta
+            randomdataSigma_Z = sigma_z.copy()
+
+            randomdataZ[numpy.sqrt((x - params[0])**2 + (y - params[1])**2) >= \
+                    ap] = 0.
+            randomdataSigma_Z[numpy.sqrt((x - params[0])**2 + \
+                    (y - params[1])**2) >= ap] = 1.0e20
+
+            randomfit, randomcov = scipy.optimize.leastsq(func, params, \
+                    args=(1, x, y, randomdataZ, randomdataSigma_Z), \
+                    full_output=False)
+
+            ps.append(randomfit)
+
+        ps = numpy.array(ps)
+
+        pfit_bootstrap = numpy.mean(ps,0)
+        perr_bootstrap = numpy.std(ps,0)
+
+        pfit_bootstrap[4] = numpy.fmod(numpy.fmod(pfit_bootstrap[4], numpy.pi)+\
+                numpy.pi, numpy.pi)
+
         # Fix the least squares result for phi because it seems to like to go
         # crazy.
 
@@ -198,7 +230,9 @@ def find(image, threshold=5, include_radius=20, window_size=40, \
 
         new_source = numpy.empty((16,), dtype=p.dtype)
         new_source[0:12][0::2] = p[0:6]
-        new_source[0:12][1::2] = sigma_p[0:6]
+        #new_source[0:12][1::2] = sigma_p[0:6]
+        #new_source[0:12][0::2] = pfit_bootstrap[0:6]
+        new_source[0:12][1::2] = perr_bootstrap[0:6]
 
         # Before doing aperture photometry, find any sources within a provided 
         # radius and fit them so they can be subtracted out of the sky
@@ -355,6 +389,21 @@ def find(image, threshold=5, include_radius=20, window_size=40, \
                     (flux_unc * sources['Flux'])**2)
 
     return sources
+
+def interferometer_noise(size, sigma, beam):
+
+    test = numpy.random.normal(0.0, sigma, size**2).reshape( \
+            (size,size))
+
+    x, y = numpy.meshgrid(numpy.arange(size),numpy.arange(size))
+
+    gaus = gaussian2d(x, y, [size/2, size/2, beam[0], beam[1], \
+            beam[2], 1])
+
+    new_test = scipy.signal.fftconvolve(test, gaus, mode='same')
+    new_test *= sigma / new_test.std()
+
+    return new_test
 
 def gaussian2d(x, y, params, n=1):
 
