d662962bf7f1ff20a521471a427290ec3d5d8279
Patrick Sheehan <psheehan287@gmail.com>
2016-02-23 15:37:51 -0700

    Added functionality for reading/writing hdf5 files with masks, and for overwriting files without needing to specify.

--- pdspy/table/table.py
+++ pdspy/table/table.py
@@ -1,18 +1,37 @@
 import astropy.table
+import h5py
+import os
 
 class Table(astropy.table.Table):
 
-    def read_hdf5(self, *args, **kwargs):
-        return
+    @staticmethod
+    def read_hdf5(*args, **kwargs):
+        try:
+            self = Table(astropy.table.Table.read(*args, **kwargs, \
+                    path="table"), masked=True)
+            self.mask = astropy.table.Table.read(*args, **kwargs, path="mask")
+        except OSError:
+            self = Table(astropy.table.Table.read(*args, **kwargs, \
+                    path="table"))
+
+        for col in self.colnames:
+            if 'S' in self[col].dtype.str:
+                self.replace_column(col, self[col].astype(str))
+
+        return self
 
     def write_hdf5(self, *args, **kwargs):
         for col in self.colnames:
             if 'U' in self[col].dtype.str:
                 self.replace_column(col, self[col].astype(bytes))
 
-        self.write(*args, **kwargs)
+        self.write(*args, **kwargs, path='table', overwrite=True)
 
         for col in self.colnames:
             if 'S' in self[col].dtype.str:
                 self.replace_column(col, self[col].astype(str))
+
+        if self.masked:
+            self.mask.write(*args, **kwargs, path='mask', append=True)
+
         return
