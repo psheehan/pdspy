df54f7d0388c06cc0c5cec714d5c4432b351e39a
Patrick Sheehan <psheehan287@gmail.com>
2016-05-11 16:29:01 -0700

    Rearranged some text to make older versions of python happy (apparently).

--- pdspy/table/table.py
+++ pdspy/table/table.py
@@ -16,12 +16,11 @@ class Table(astropy.table.Table):
     @staticmethod
     def read_hdf5(*args, **kwargs):
         try:
-            self = Table(astropy.table.Table.read(*args, **kwargs, \
-                    path="table"), masked=True)
-            self.mask = astropy.table.Table.read(*args, **kwargs, path="mask")
+            self = Table(astropy.table.Table.read(*args, path="table", \
+                    **kwargs), masked=True)
+            self.mask = astropy.table.Table.read(*args, path="mask", **kwargs)
         except OSError:
-            self = Table(astropy.table.Table.read(*args, **kwargs, \
-                    path="table"))
+            self = Table(astropy.table.Table.read(*args, path="table",**kwargs))
 
         for col in self.colnames:
             if 'S' in self[col].dtype.str:
@@ -34,13 +33,13 @@ class Table(astropy.table.Table):
             if 'U' in self[col].dtype.str:
                 self.replace_column(col, self[col].astype(bytes))
 
-        self.write(*args, **kwargs, path='table', overwrite=True)
+        self.write(*args, path='table', overwrite=True, **kwargs)
 
         for col in self.colnames:
             if 'S' in self[col].dtype.str:
                 self.replace_column(col, self[col].astype(str))
 
         if self.masked:
-            self.mask.write(*args, **kwargs, path='mask', append=True)
+            self.mask.write(*args, path='mask', append=True, **kwargs)
 
         return
