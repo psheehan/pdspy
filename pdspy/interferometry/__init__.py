e395745f2bb3a95a6df7d08009c2db123faf94c4
Patrick Sheehan <psheehan287@gmail.com>
2016-08-09 09:41:38 -0700

    Updated fit_model_new to work fully to fit a single model to data. Also, made the fit_model from fit_model_new to be the default fitter. Will make fit_model_new into fit_model when it is fully operational.

--- pdspy/interferometry/__init__.py
+++ pdspy/interferometry/__init__.py
@@ -4,7 +4,7 @@ from .readvis import readvis
 from .center import center
 from .clean import clean
 from .concatenate import concatenate
-from .fit_model import fit_model
+from .fit_model_new import fit_model
 from .invert import invert
 from .model import model
 from .rotate import rotate
--- pdspy/interferometry/fit_model_new.py
+++ pdspy/interferometry/fit_model_new.py
@@ -1,8 +1,8 @@
 import numpy
 import emcee
 import matplotlib.pyplot as plt
+import scipy.signal
 from .model import model
-from ..mcmc import mcmc2d
 
 def fit_model(data, funct='point', nsteps=1e3, niter=3):
 
@@ -27,6 +27,9 @@ def fit_model(data, funct='point', nsteps=1e3, niter=3):
     y = numpy.arange(-5,5,0.1)
 
     params = numpy.array([])
+
+    xmin = []
+    ymin = []
 
     for k in range(funct.size):
         chisq = numpy.zeros((x.size, y.size))
@@ -54,21 +57,8 @@ def fit_model(data, funct='point', nsteps=1e3, niter=3):
 
                 chisq[i,j] = calc_chisq(data, par, funct[0:k+1])
     
-        xmin = x[numpy.where(chisq == chisq.min())[0][0]]
-        ymin = y[numpy.where(chisq == chisq.min())[1][0]]
-
-        if (funct[k] == 'point'):
-            params = numpy.concatenate((params, numpy.array([xmin, \
-                    ymin, flux0])))
-        elif (funct[k] == 'gauss'):
-            params = numpy.concatenate((params, numpy.array([xmin, ymin, \
-                    0.1, 0.1, 0.0, flux0])))
-        elif (funct[k] == 'circle'):
-            params = numpy.concatenate((params, numpy.array([xmin, ymin, \
-                    0.1, 0.0, 0.0, flux0])))
-        elif (funct[k] == 'ring'):
-            params = numpy.concatenate((params, numpy.array([xmin, ymin, \
-                    0.1, 0.2, 0.0, 0.0, flux0])))
+        xmin.append(x[numpy.where(chisq == chisq.min())[0][0]])
+        ymin.append(y[numpy.where(chisq == chisq.min())[1][0]])
 
     # Next do a few iterations of MCMC to get the correct solution.
 
@@ -77,120 +67,171 @@ def fit_model(data, funct='point', nsteps=1e3, niter=3):
     z = numpy.concatenate((data.real, data.imag))[:,0]
     zerr = 1./numpy.sqrt(numpy.concatenate((data.weights,data.weights)))[:,0]
 
-    args = {'return_type':'append', 'funct':funct}
-
     # Define a likelihood function.
 
-    def lnlike(p, x, y, z, zerr):
-        m = model(x, y, params, return_type="append", funct=funct)
+    pa0 = numpy.pi
+    pa_range = numpy.pi
 
-        return -0.5*(numpy.sum((z - m)**2 / zerr**2))
+    for count in range(2):
+        def lnlike(p, x, y, z, zerr, funct):
+            m = model(x, y, p, return_type="append", funct=funct)
 
-    if funct[0] == 'point':
-        def lnprior(p):
-            if 0. < p[2]:
-                return 0.0
+            return -0.5*(numpy.sum((z - m)**2 / zerr**2))
 
-            return -numpy.inf
-    elif funct[0] == 'gauss':
-        def lnprior(p):
-            if 0. < p[2] and 0. < p[3] < p[2] and -numpy.pi/2 <= p[4] <= numpy.pi/2 and \
-                    0. < p[5]:
-                return 0.0
+        if funct[0] == 'point':
+            def lnprior(p):
+                if 0. < p[2]:
+                    return 0.0
 
-            return -numpy.inf
-    elif funct[0] == 'circle':
-        def lnprior(p):
-            if 0. < p[2] and 0. <= p[3] <= numpy.pi/2 and \
-                    0.0 <= p[4] <= numpy.pi and 0. < p[5]:
-                return 0.0
+                return -numpy.inf
+        elif funct[0] == 'gauss':
+            def lnprior(p):
+                if 0 < p[2] and 0 < p[3] <= p[2] and pa0-pa_range<= p[4] <= \
+                        pa0+pa_range and 0. < p[5]:
+                    return 0.0
 
-            return -numpy.inf
-    elif funct[0] == 'ring':
-        def lnprior(p):
-            if 0. < p[2] and p[2] < p[3] and 0.0 <= p[4] <= numpy.pi/2 and \
-                    0 <= p[5] <= numpy.pi and 0. < p[6]:
-                return 0.0
+                return -numpy.inf
+        elif funct[0] == 'circle':
+            def lnprior(p):
+                if 0. < p[2] and 0. <= p[3] <= numpy.pi/2 and \
+                        pa0-pa_range <= p[4] <= pa0+pa_range and 0. < p[5]:
+                    return 0.0
 
-            return -numpy.inf
+                return -numpy.inf
+        elif funct[0] == 'ring':
+            def lnprior(p):
+                if 0. < p[2] and p[2] < p[3] and 0.0 <= p[4] <= numpy.pi/2 and \
+                        pa0-pa_range <= p[5] <= pa0+pa_range and 0. < p[6]:
+                    return 0.0
 
-    def lnprob(p, x, y, z, zerr):
-        lp = lnprior(p)
+                return -numpy.inf
 
-        if not numpy.isfinite(lp):
-            return -numpy.inf
+        def lnprob(p, x, y, z, zerr, funct):
+            lp = lnprior(p)
 
-        return lp + lnlike(p, x, y, z, zerr)
+            if not numpy.isfinite(lp):
+                return -numpy.inf
 
-    # Set up the emcee run.
+            return lp + lnlike(p, x, y, z, zerr, funct)
 
-    if funct[0] == 'point':
-        ndim, nwalkers = 2, 250
+        # Set up the emcee run.
 
-        pos = []
-        for i in range(nwalkers):
-            pos.append([numpy.random.normal(xmin,0.1,1)[0], \
-                    numpy.random.normal(xmin,0.1,1)[0], \
-                    numpy.random.uniform(0.0001,1.0,1)[0]])
-    elif funct[0] == 'gauss':
-        ndim, nwalkers = 6, 250
+        if count == 0:
+            if funct[0] == 'point':
+                ndim, nwalkers = 2, 250
 
-        pos = []
-        for i in range(nwalkers):
-            sigma_x = numpy.random.uniform(0.01,1.0,1)[0]
+                pos = []
+                for i in range(nwalkers):
+                    pos.append([numpy.random.normal(xmin[0],0.1,1)[0], \
+                            numpy.random.normal(xmin[0],0.1,1)[0], \
+                            numpy.random.uniform(0.0001,1.0,1)[0]])
+            elif funct[0] == 'gauss':
+                ndim, nwalkers = 6, 250
 
-            pos.append([numpy.random.normal(xmin,1.0e-4,1)[0], \
-                    numpy.random.normal(xmin,1.0e-4,1)[0], \
-                    sigma_x, \
-                    numpy.random.uniform(0.01,sigma_x,1)[0], \
-                    numpy.random.uniform(-numpy.pi/2, numpy.pi/2, 1)[0], \
-                    numpy.random.uniform(0.0001,1.0,1)[0]])
-    elif funct[0] == 'circle':
-        ndim, nwalkers = 6, 250
+                pos = []
+                for i in range(nwalkers):
+                    sigma_x = numpy.random.uniform(0.01,10.0,1)[0]
 
-        pos = []
-        for i in range(nwalkers):
-            pos.append([numpy.random.normal(xmin,0.1,1)[0], \
-                    numpy.random.normal(xmin,0.1,1)[0], \
-                    numpy.random.uniform(0.01,1.0,1)[0], \
-                    numpy.random.uniform(0,numpy.pi/2,1)[0], \
-                    numpy.random.uniform(0,numpy.pi,1)[0], \
-                    numpy.random.uniform(0.0001,1.0,1)[0]])
-    elif funct[0] == 'ring':
-        ndim, nwalkers = 6, 250
+                    pos.append([numpy.random.normal(xmin[0],1.0e-4,1)[0], \
+                            numpy.random.normal(xmin[0],1.0e-4,1)[0], \
+                            sigma_x, \
+                            numpy.random.uniform(0.01,sigma_x,1)[0], \
+                            numpy.random.uniform(pa0-pa_range,\
+                            pa0+pa_range,1)[0],\
+                            numpy.random.normal(flux0,1.0e-4,1)[0]])
+            elif funct[0] == 'circle':
+                ndim, nwalkers = 6, 250
 
-        pos = []
-        for i in range(nwalkers):
-            r_min = numpy.random.uniform(0.01,1.0,1)[0]
+                pos = []
+                for i in range(nwalkers):
+                    pos.append([numpy.random.normal(xmin[0],1.0e-4,1)[0], \
+                            numpy.random.normal(xmin[0],1.0e-4,1)[0], \
+                            numpy.random.uniform(0.01,10.0,1)[0], \
+                            numpy.random.uniform(0,numpy.pi/2,1)[0], \
+                            numpy.random.uniform(pa0-pa_range,\
+                            pa0+pa_range,1)[0], \
+                            numpy.random.uniform(flux0,1.0e-4,1)[0]])
+            elif funct[0] == 'ring':
+                ndim, nwalkers = 6, 250
 
-            pos.append([numpy.random.normal(xmin,0.1,1)[0], \
-                    numpy.random.normal(xmin,0.1,1)[0], \
-                    r_min, \
-                    numpy.random.uniform(r_min,1.0,1)[0], \
-                    numpy.random.uniform(0,numpy.pi/2,1)[0], \
-                    numpy.random.uniform(0,numpy.pi,1)[0], \
-                    numpy.random.uniform(0.0001,1.0,1)[0]])
+                pos = []
+                for i in range(nwalkers):
+                    r_min = numpy.random.uniform(0.01,1.0,1)[0]
 
-    # Set up the MCMC simulation.
+                    pos.append([numpy.random.normal(xmin[0],0.1,1)[0], \
+                            numpy.random.normal(xmin[0],0.1,1)[0], \
+                            r_min, \
+                            numpy.random.uniform(r_min,1.0,1)[0], \
+                            numpy.random.uniform(0,numpy.pi/2,1)[0], \
+                            numpy.random.uniform(pa0-pa_range,\
+                            pa0+pa_range,1)[0], \
+                            numpy.random.uniform(0.0001,1.0,1)[0]])
 
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, \
-            args=(x, y, z, zerr))
+        # Set up the MCMC simulation.
 
-    # Run a frew burner steps.
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, \
+                args=(x, y, z, zerr, funct))
 
-    pos, prob, state = sampler.run_mcmc(pos, 50)
+        # Run a frew burner steps.
 
-    for i in range(ndim):
-        fig, ax = plt.subplots(nrows=1, ncols=1)
+        if count == 0:
+            pos, prob, state = sampler.run_mcmc(pos, 250)
+        else:
+            pos, prob, state = sampler.run_mcmc(pos, 50)
 
-        for j in range(nwalkers):
-            ax.plot(sampler.chain[j,:,i])
+        # Make a plot of the steps.
 
-        plt.show()
+        for i in range(ndim):
+            fig, ax = plt.subplots(nrows=1, ncols=1)
 
-        plt.close(fig)
+            for j in range(nwalkers):
+                ax.plot(sampler.chain[j,:,i])
 
-    sampler.reset()
+            plt.show()
+
+            plt.close(fig)
+
+        # Because of the bimodality of p.a. we need to fix where the p.a. 
+        # window is centered on.
+
+        if funct[0] == "point":
+            pass
+        else:
+            if count == 0:
+                if funct[0] == "gauss" or funct[0] == "circle":
+                    ind = 4
+                elif funct[0] == "ring":
+                    ind = 5
+
+                samples = sampler.chain.reshape((-1,ndim))[:,ind]
+                pa_hist, bins = numpy.histogram(samples, 20)
+
+                bin_centers = (bins[0:-1] + bins[1:]) / 2.
+
+                extrema = scipy.signal.argrelextrema(pa_hist, numpy.greater)[0]
+                pa0 = bin_centers[extrema[0]]
+                e_current = extrema[0]
+                for  e in extrema[1:]:
+                    if pa_hist[e] > pa_hist[e_current]:
+                        pa0 = bin_centers[e]
+                        e_current = e
+
+                pa0 = numpy.fmod(numpy.fmod(pa0, numpy.pi)+numpy.pi, numpy.pi)
+                pa_range = numpy.pi/2
+
+                # Put all the walkers into the correct p.a. range.
+
+                for position in pos:
+                    while position[ind] < pa0 - numpy.pi/2 or \
+                            position[ind] >= pa0 + numpy.pi/2:
+                        if position[ind] < pa0 - numpy.pi/2:
+                            position[ind] += numpy.pi
+                        elif position[ind] >= pa0 + numpy.pi/2:
+                            position[ind] -= numpy.pi
+
+        # Reset the sampler.
+
+        sampler.reset()
 
     # Run the real MCMC simulation.
 
@@ -207,11 +248,10 @@ def fit_model(data, funct='point', nsteps=1e3, niter=3):
 
     for k in range(2):
         good = numpy.repeat(True, samples.shape[0])
-        print(samples.shape)
         if k == 0:
-            nsigma=3.
+            nsigma = 3.
         else:
-            nsigma=4.
+            nsigma = 4.
 
         for i in range(good.size):
             for j in range(ndim):
@@ -223,6 +263,7 @@ def fit_model(data, funct='point', nsteps=1e3, niter=3):
 
         params = samples.mean(axis=0)
         sigma = samples.std(axis=0)
-    print(samples.shape)
+
+    # Clip a few stragglers and re-calculate.
 
     return params, sigma, samples
