de59b6821cd64400856ef449b29e03691a02f63e
Patrick Sheehan <psheehan287@gmail.com>
2017-08-18 14:36:04 -0600

    Updated interpolate_model to be both faster and more accurate.

--- pdspy/interferometry/interpolate_model.py
+++ pdspy/interferometry/interpolate_model.py
@@ -4,16 +4,20 @@ from .libinterferometry import Visibilities
 
 def interpolate_model(u, v, freq, model):
 
-    uuvv = numpy.transpose(numpy.vstack((model.u, model.v)))
+    nu = int(model.u.size**0.5)
+    model_u = model.u[0:nu]
+    model_v = model.v[0:-1:nu]
 
     real = []
     imag = []
 
     for i in range(len(model.freq)):
-        real.append(scipy.interpolate.griddata(uuvv, model.real[:,i], \
-                (u*freq[i]/freq.mean(), v*freq[i]/freq.mean()), method='cubic'))
-        imag.append(scipy.interpolate.griddata(uuvv, model.imag[:,i], \
-                (u*freq[i]/freq.mean(), v*freq[i]/freq.mean()), method='cubic'))
+        f = scipy.interpolate.RectBivariateSpline(model_u, model_v, \
+                model.real[:,i].reshape((nu,nu)), kx=5, ky=5)
+        real.append(f.ev(u*freq[i]/freq.mean(), v*freq[i]/freq.mean()))
+        f = scipy.interpolate.RectBivariateSpline(model_u, model_v, \
+                model.imag[:,i].reshape((nu,nu)), kx=5, ky=5)
+        imag.append(f.ev(u*freq[i]/freq.mean(), v*freq[i]/freq.mean()))
 
     real = numpy.transpose(numpy.array(real))
     imag = numpy.transpose(numpy.array(imag))
