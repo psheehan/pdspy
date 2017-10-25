ae5fe85dceb9778bc36b606accba5eefd91444eb
Patrick Sheehan <psheehan@Wen-fais-iMac.local>
2017-10-24 19:32:53 -0500

    Added a few parameters that are needed for spectral line modeling.

--- bin/config_template.py
+++ bin/config_template.py
@@ -101,6 +101,10 @@ parameters = {
         "f_M_large":{"fixed":True, "value":0.8, "limits":[0.05, 1.]},
         "f_h_large":{"fixed":True, "value":0.5, "limits":[0.1, 1.]},
         "beta_large":{"fixed":True, "value":1., "limits":[0.5, 1.5]},
+        # Disk temperature parameters.
+        "logT0":{"fixed":True, "value":2.5, "limits":[1.,3.5]},
+        "q":{"fixed":True, "value":0.25, "limits":[0.,1.5]},
+        "loga_turb":{"fixed":True, "value":-1.0, "limits":[-3.,1.]},
         # Envelope parameters.
         "logM_env":{"fixed":True, "value":-3., "limits":[-10., -2.]},
         "logR_in_env":{"fixed":True, "value":"logR_in", "limits":[-1., 4.]},
@@ -118,4 +122,5 @@ parameters = {
         "y0":{"fixed":True, "value":0., "limits":[-0.1,0.1]},
         "dpc":{"fixed":True, "value":140., "limits":[1.,1e6]},
         "Ak":{"fixed":True, "value":0., "limits":[0.,1.]},
+        "v_sys":{"fixed":True, "value":5., "limits":[0.,10.]},
         }
