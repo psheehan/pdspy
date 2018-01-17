d9f6749230307dd649e8902bc9bb4fc15c77a46e
nickalaskreynolds <nickalaskreynolds@gmail.com>
2018-01-17 12:15:46 -0600

    fixing file naming schema and property conflict

--- bin/UVFITS_to_HDF5.py
+++ bin/UVFITS_to_HDF5.py
@@ -8,17 +8,15 @@ TIME=str(time)
 import pdspy.interferometry as uv
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input',dest='in')
+parser.add_argument('-i', '--input',dest='inp')
 parser.add_argument('-o', '--output',dest='out',default='data_{}.hdf5'.format(TIME))
 args = parser.parse_args()
-args.out = args.out.strip('.hdf5') + '.hdf5'
 
-print('Using Directory: {}'.format(args.in))
-files=glob.glob('{}.vis'.format(args.in))
+print('Using Directory: {}'.format(args.inp))
+files=glob.glob('{}'.format(args.inp))
 
 data = []
 for file in files:
-    print('Reading in file: {}'.format(file))
     data.append(uv.readvis(file))
 
 data[-1].weights[data[-1].uvdist < 50000] = 0.
