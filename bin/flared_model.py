7a26b5d4a714e22991bbc1af0d9d14f482df1148
Patrick Sheehan <psheehan287@gmail.com>
2018-01-19 14:35:15 -0600

    Adjusted the way that the initial positions of the walkers in logR_disk space are set.

--- bin/flared_model.py
+++ bin/flared_model.py
@@ -534,10 +534,11 @@ if args.resume:
 else:
     pos = []
     for j in range(nwalkers):
-        r_env = numpy.random.uniform(2.5,4.,1)[0]
+        r_env = numpy.random.uniform(parameters["logR_env"]["limits"][0],\
+                parameters["logR_env"]["limits"][1],1)[0]
         r_disk = numpy.random.uniform(numpy.log10(5.),\
-                numpy.log10(10.**r_env),1)[0]
-        r_in = numpy.random.uniform(numpy.log10(0.1),\
+                min(r_env, parameters["logR_disk"]["limits"][1]),1)[0]
+        r_in = numpy.random.uniform(parameters["logR_in"]["limits"][0],\
                 numpy.log10((10.**r_disk)/2),1)[0]
 
         r_cav = numpy.random.uniform(r_in, numpy.log10(0.75*10.**r_disk),1)[0]
