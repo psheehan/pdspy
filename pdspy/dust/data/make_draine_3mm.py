c231d11f008e5818c81c9b05b1f4a2483e74efd2
Patrick Sheehan <psheehan287@gmail.com>
2018-02-07 17:29:33 -0600

    Fixed a problem found in the Draine opacity calculations and updated the files.

--- pdspy/dust/data/make_draine_3mm.py
+++ pdspy/dust/data/make_draine_3mm.py
@@ -7,26 +7,26 @@ water_ice = Dust()
 water_ice.set_optical_constants_from_henn("optical_constants/water_ice.txt")
 water_ice.set_density(0.92)
 
+#1/3
 graphite_parallel = Dust()
-graphite_parallel.set_optical_constants_from_draine("optical_constants/astronomical_silicates.txt")
+graphite_parallel.set_optical_constants_from_draine("optical_constants/graphite_parallel_0.01.txt")
 graphite_parallel.set_density(2.24)
 graphite_parallel.calculate_optical_constants_on_wavelength_grid(water_ice.lam)
 
 #2/3
 graphite_perpendicular = Dust()
-graphite_perpendicular.set_optical_constants_from_draine("optical_constants/astronomical_silicates.txt")
+graphite_perpendicular.set_optical_constants_from_draine("optical_constants/graphite_perpendicular_0.01.txt")
 graphite_perpendicular.set_density(2.24)
 graphite_perpendicular.calculate_optical_constants_on_wavelength_grid(\
         water_ice.lam)
 
-#1/3
 silicates = Dust()
 silicates.set_optical_constants_from_draine("optical_constants/astronomical_silicates.txt")
 silicates.set_density(3.3)
 silicates.calculate_optical_constants_on_wavelength_grid(water_ice.lam)
 
 species = [silicates,graphite_parallel,graphite_perpendicular]
-abundances = numpy.array([65.,25.*1./3,65.*2./3])
+abundances = numpy.array([65.,35.*1./3,35.*2./3])
 print(abundances)
 
 dust = mix_dust(species, abundances)
@@ -35,7 +35,7 @@ amin = 0.005e-4
 amax = 3.000e-1
 pl = 3.5
 
-dust.calculate_size_distribution_opacity(amin, amax, pl, \
+dust.calculate_size_distribution_opacity(amin, amax, pl, nang=1, \
         coat_volume_fraction=0.0)
 
 dust.write('draine_3mm.hdf5')
