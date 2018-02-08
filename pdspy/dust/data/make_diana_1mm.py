43f803bd79d255eef242a51b5f9943f62c62d6ab
Patrick Sheehan <psheehan287@gmail.com>
2018-02-08 13:53:18 -0600

    Added opacities following the perscription from the DIANA project for a range of maximum wavelengths.

--- pdspy/dust/data/make_diana_1mm.py
+++ pdspy/dust/data/make_diana_1mm.py
@@ -18,10 +18,10 @@ abundances = numpy.array([0.8,0.2])
 dust = mix_dust(species, abundances, filling=0.75)
 
 amin = 0.05e-4
-amax = 1.000e-4
+amax = 1.000e-1
 pl = 3.5
 
 dust.calculate_size_distribution_opacity(amin, amax, pl, with_dhs=True, \
         coat_volume_fraction=0.0, nang=1)
 
-dust.write('diana_1um.hdf5')
+dust.write('diana_1mm.hdf5')
