7ec47d008aca9d54b066d415ca49d4e87183fec7
Patrick Sheehan <psheehan287@gmail.com>
2018-07-03 20:31:25 -0500

    Added a program to calculate model photometry from the BTSettl grid of models.

--- pdspy/spectroscopy/__init__.py
+++ pdspy/spectroscopy/__init__.py
@@ -2,3 +2,4 @@ from .spectroscopy import Spectrum
 from .find_lines import find_lines
 from .line_flux import line_flux
 from .read_spectrum import read_spectrum
+from .btsettl_photometry import btsettl_photometry
---
+++ pdspy/spectroscopy/btsettl_photometry.py
@@ -0,0 +1,81 @@
+from pdspy.constants.astronomy import R_sun, pc
+import scipy.interpolate
+import astropy.table
+import numpy
+
+def btsettl_photometry(Teff=5770., Logg=4.4, Rstar=1.0, dpc=140., \
+        system="Johnson", filters="B", AB=False):
+
+    # Create the filename.
+
+    path = '/'.join(__file__.split("/")[0:-1])+"/btsettl_data/"
+
+    basename = "colmag.BT-Settl.server."
+
+    filename = basename + system + "."
+
+    if AB:
+        filename += "AB.txt"
+    else:
+        filename += "Vega.txt"
+
+    # Get the column names.
+
+    f = open(path+filename, "r")
+    lines = f.readlines()
+    f.close()
+
+    for i, line in enumerate(lines):
+        if line[0] != "!":
+            break
+
+    colnames = lines[i-1].split()[1:]
+
+    # Get the data.
+
+    data = numpy.loadtxt(path+filename, comments='!')
+
+    # Make the data into a table.
+
+    table = astropy.table.Table(data[1:,0:16], names=colnames)
+
+    # Make sure we only include the Teff's with all the log(g)'s.
+
+    teff = numpy.unique(table["Teff"])
+    logg = numpy.unique(table["Logg"])
+
+    for T in teff:
+        if len(table["Teff"][table["Teff"] == T]) < len(logg):
+            table.remove_rows(numpy.where(table["Teff"] == T)[0])
+
+    # Now loop through the filters and get the photometry.
+
+    teff = numpy.array(numpy.unique(table["Teff"]))
+    logg = numpy.array(numpy.unique(table["Logg"]))
+
+    mag = []
+
+    for filter in filters.split():
+        data = numpy.array(table[filter]).reshape((teff.size, logg.size))
+
+        # Now do the 2D interpolation.
+
+        f = scipy.interpolate.interp2d(logg, teff, data)
+
+        # Calculate the photometry now.
+
+        mag.append(f(Logg, Teff)[0])
+
+    # Turn the photometry into an array.
+
+    mag = numpy.array(mag)
+
+    # Turn into an absolute magnitude.
+
+    M = mag - 5 * numpy.log10(Rstar*R_sun / pc) + 5.
+
+    # Now get the apparent magnitude.
+
+    photometry = M + 5*(numpy.log10(dpc) - 1.)
+
+    return photometry
