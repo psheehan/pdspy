14aeb96638672a740d0dfbb14f876ecc846911d6
Patrick Sheehan <psheehan287@gmail.com>
2019-01-21 17:39:39 -0500

    Updated flared_model_ptsampler to use the new functions provided elsewhere, and to keep it up to date with flared_model

--- bin/flared_model_ptsampler.py
+++ bin/flared_model_ptsampler.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python3
 
 from pdspy.constants.physics import c, m_p, G
-from pdspy.constants.physics import k as k_b
-from pdspy.constants.astronomy import M_sun, AU
 from matplotlib.backends.backend_pdf import PdfPages
 import pdspy.modeling.mpi_pool
 import pdspy.interferometry as uv
 import pdspy.spectroscopy as sp
 import pdspy.modeling as modeling
 import pdspy.imaging as im
-import pdspy.misc as misc
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.patches as patches
 import matplotlib.patheffects as PathEffects
-import pdspy.table
-import pdspy.dust as dust
-import pdspy.gas as gas
-import pdspy.mcmc as mc
-import scipy.signal
 import argparse
 import numpy
-import time
 import sys
 import os
 import emcee
@@ -86,282 +77,12 @@ else:
 #
 ################################################################################
 
-def model(visibilities, params, parameters, plot=False):
-
-    # Set the values of all of the parameters.
-
-    p = {}
-    for key in parameters:
-        if parameters[key]["fixed"]:
-            if parameters[key]["value"] in parameters.keys():
-                if parameters[parameters[key]["value"]]["fixed"]:
-                    value = parameters[parameters[key]["value"]]["value"]
-                else:
-                    value = params[parameters[key]["value"]]
-            else:
-                value = parameters[key]["value"]
-        else:
-            value = params[key]
-
-        if key[0:3] == "log":
-            p[key[3:]] = 10.**value
-        else:
-            p[key] = value
-
-    # Make sure alpha and beta are defined.
-
-    if p["disk_type"] in ["exptaper","dartois-exptaper"]:
-        t_rdisk = p["T0"] * (p["R_disk"] / 1.)**-p["q"]
-        p["h_0"] = ((k_b*(p["R_disk"]*AU)**3*t_rdisk) / (G*p["M_star"]*M_sun * \
-                2.37*m_p))**0.5 / AU
-    else:
-        p["h_0"] = ((k_b * AU**3 * p["T0"]) / (G*p["M_star"]*M_sun * \
-                2.37*m_p))**0.5 / AU
-    p["beta"] = 0.5 * (3 - p["q"])
-    p["alpha"] = p["gamma"] + p["beta"]
-
-    # Set up the dust.
-
-    dustopac = p["dust_file"]
-
-    dust_gen = dust.DustGenerator(dust.__path__[0]+"/data/"+dustopac)
-
-    ddust = dust_gen(p["a_max"] / 1e4, p["p"])
-    edust = dust_gen(1.0e-4, 3.5)
-
-    # Set up the gas.
-
-    gases = []
-    abundance = []
-
-    index = 1
-    while index > 0:
-        if "gas_file"+str(index) in p:
-            g = gas.Gas()
-            g.set_properties_from_lambda(gas.__path__[0]+"/data/"+\
-                    p["gas_file"+str(index)])
-
-            gases.append(g)
-            abundance.append(p["abundance"+str(index)])
-
-            index += 1
-        else:
-            index = -1
-
-    # Make sure we are in a temp directory to not overwrite anything.
-
-    original_dir = os.environ["PWD"]
-    os.mkdir("/tmp/temp_{1:s}_{0:d}".format(comm.Get_rank(), source))
-    os.chdir("/tmp/temp_{1:s}_{0:d}".format(comm.Get_rank(), source))
-
-    # Write the parameters to a text file so it is easy to keep track of them.
-
-    f = open("params.txt","w")
-    for key in p:
-        f.write("{0:s} = {1}\n".format(key, p[key]))
-    f.close()
-
-    # Set up the model. 
-
-    m = modeling.YSOModel()
-    m.add_star(mass=p["M_star"], luminosity=p["L_star"],temperature=p["T_star"])
-
-    if p["envelope_type"] == "ulrich":
-        m.set_spherical_grid(p["R_in"], p["R_env"], 100, 51, 2, code="radmc3d")
-    else:
-        m.set_spherical_grid(p["R_in"], max(5*p["R_disk"],300), 100, 51, 2, \
-                code="radmc3d")
-
-    if p["disk_type"] == "exptaper":
-        m.add_pringle_disk(mass=p["M_disk"], rmin=p["R_in"], rmax=p["R_disk"], \
-                plrho=p["alpha"], h0=p["h_0"], plh=p["beta"], dust=ddust, \
-                t0=p["T0"], plt=p["q"], gas=gases, abundance=abundance,\
-                aturb=p["a_turb"])
-    elif p["disk_type"] == "dartois-exptaper":
-        m.add_pringle_disk(mass=p["M_disk"], rmin=p["R_in"], rmax=p["R_disk"], \
-                plrho=p["alpha"], h0=p["h_0"], plh=p["beta"], dust=ddust, \
-                tmid0=p["tmid0"], tatm0=p["tatm0"], zq0=p["zq0"], \
-                pltgas=p["pltgas"], delta=p["delta"], gas=gases, \
-                abundance=abundance, aturb=p["a_turb"])
-    elif p["disk_type"] == "dartois-truncated":
-        m.add_disk(mass=p["M_disk"], rmin=p["R_in"], rmax=p["R_disk"], \
-                plrho=p["alpha"], h0=p["h_0"], plh=p["beta"], dust=ddust, \
-                tmid0=p["tmid0"], tatm0=p["tatm0"], zq0=p["zq0"], \
-                pltgas=p["pltgas"], delta=p["delta"], gas=gases, \
-                abundance=abundance, aturb=p["a_turb"])
-    else:
-        m.add_disk(mass=p["M_disk"], rmin=p["R_in"], rmax=p["R_disk"], \
-                plrho=p["alpha"], h0=p["h_0"], plh=p["beta"], dust=ddust, \
-                t0=p["T0"], plt=p["q"], gas=gases, abundance=abundance,\
-                aturb=p["a_turb"])
-
-    if p["envelope_type"] == "ulrich":
-        m.add_ulrich_envelope(mass=p["M_env"], rmin=p["R_in"], rmax=p["R_env"],\
-                cavpl=p["ksi"], cavrfact=p["f_cav"], dust=edust, \
-                t0=p["T0_env"], tpl=p["q_env"], gas=gases, abundance=abundance,\
-                aturb=p["a_turb_env"])
-    else:
-        pass
-
-    m.grid.set_wavelength_grid(0.1,1.0e5,500,log=True)
-
-    # Run the images/visibilities/SEDs.
-
-    for j in range(len(visibilities["file"])):
-        # Shift the wavelengths by the velocities.
-
-        b = p["v_sys"]*1.0e5 / c
-        lam = c / visibilities["data"][j].freq / 1.0e-4
-        wave = lam * numpy.sqrt((1. - b) / (1. + b))
-
-        # Set the wavelengths for RADMC3D to use.
-
-        m.set_camera_wavelength(wave)
-
-        if p["docontsub"]:
-            m.run_image(name=visibilities["lam"][j], nphot=1e5, \
-                    npix=visibilities["npix"][j], lam=None, \
-                    pixelsize=visibilities["pixelsize"][j], tgas_eq_tdust=True,\
-                    scattering_mode_max=0, incl_dust=True, incl_lines=True, \
-                    loadlambda=True, incl=p["i"], pa=p["pa"], dpc=p["dpc"], \
-                    code="radmc3d", verbose=False, writeimage_unformatted=True,\
-                    setthreads=ncpus)
-
-            m.run_image(name="cont", nphot=1e5, \
-                    npix=visibilities["npix"][j], lam=None, \
-                    pixelsize=visibilities["pixelsize"][j], tgas_eq_tdust=True,\
-                    scattering_mode_max=0, incl_dust=True, incl_lines=False, \
-                    loadlambda=True, incl=p["i"], pa=p["pa"], dpc=p["dpc"], \
-                    code="radmc3d", verbose=False, writeimage_unformatted=True,\
-                    setthreads=ncpus)
-
-            m.images[visibilities["lam"][j]].image -= m.images["cont"].image
-        else:
-            m.run_image(name=visibilities["lam"][j], nphot=1e5, \
-                    npix=visibilities["npix"][j], lam=None, \
-                    pixelsize=visibilities["pixelsize"][j], tgas_eq_tdust=True,\
-                    scattering_mode_max=0, incl_dust=False, incl_lines=True, \
-                    loadlambda=True, incl=p["i"], pa=p["pa"], dpc=p["dpc"], \
-                    code="radmc3d", verbose=False, writeimage_unformatted=True,\
-                    setthreads=ncpus)
-
-        # Extinct the data, if included.
-
-        velocity = c * (float(visibilities["freq"][j])*1.0e9 - \
-                visibilities["data"][j].freq)/(float(visibilities["freq"][j])*\
-                1.0e9) / 1.0e5
-
-        tau = p["tau0"] * numpy.exp(-(velocity - p["v_ext"])**2 / \
-                (2*p["sigma_vext"]**2))
-
-        extinction = numpy.exp(-tau)
-
-        for i in range(len(m.images[visibilities["lam"][j]].freq)):
-            m.images[visibilities["lam"][j]].image[:,:,i,:] *= extinction[i]
-
-        # Invert to get the visibilities.
-
-        m.visibilities[visibilities["lam"][j]] = uv.interpolate_model(\
-                visibilities["data"][j].u, visibilities["data"][j].v, \
-                visibilities["data"][j].freq, \
-                m.images[visibilities["lam"][j]], dRA=-p["x0"], dDec=-p["y0"], \
-                nthreads=ncpus)
-
-        if plot:
-            lam = c / visibilities["image"][j].freq / 1.0e-4
-            wave = lam * numpy.sqrt((1. - b) / (1. + b))
-
-            m.set_camera_wavelength(wave)
-
-            if p["docontsub"]:
-                m.run_image(name=visibilities["lam"][j], nphot=1e5, \
-                        npix=visibilities["image_npix"][j], lam=None, \
-                        pixelsize=visibilities["image_pixelsize"][j], \
-                        tgas_eq_tdust=True, scattering_mode_max=0, \
-                        incl_dust=True, incl_lines=True, loadlambda=True, \
-                        incl=p["i"], pa=-p["pa"], dpc=p["dpc"], code="radmc3d",\
-                        verbose=False, setthreads=ncpus)
-
-                m.run_image(name="cont", nphot=1e5, \
-                        npix=visibilities["image_npix"][j], lam=None, \
-                        pixelsize=visibilities["image_pixelsize"][j], \
-                        tgas_eq_tdust=True, scattering_mode_max=0, \
-                        incl_dust=True, incl_lines=False, loadlambda=True, \
-                        incl=p["i"], pa=-p["pa"], dpc=p["dpc"], code="radmc3d",\
-                        verbose=False, setthreads=ncpus)
-
-                m.images[visibilities["lam"][j]].image -= m.images["cont"].image
-            else:
-                m.run_image(name=visibilities["lam"][j], nphot=1e5, \
-                        npix=visibilities["image_npix"][j], lam=None, \
-                        pixelsize=visibilities["image_pixelsize"][j], \
-                        tgas_eq_tdust=True, scattering_mode_max=0, \
-                        incl_dust=False, incl_lines=True, loadlambda=True, \
-                        incl=p["i"], pa=-p["pa"], dpc=p["dpc"], code="radmc3d",\
-                        verbose=False, setthreads=ncpus)
-
-            # Extinct the data, if included.
-
-            velocity = c * (float(visibilities["freq"][j])*1.0e9 - \
-                    visibilities["image"][j].freq) / \
-                    (float(visibilities["freq"][j])*1.0e9) / 1.0e5
-
-            tau = p["tau0"] * numpy.exp(-(velocity - p["v_ext"])**2 / \
-                    (2*p["sigma_vext"]**2))
-
-            extinction = numpy.exp(-tau)
-
-            for i in range(len(m.images[visibilities["lam"][j]].freq)):
-                m.images[visibilities["lam"][j]].image[:,:,i,:] *= extinction[i]
-
-            # Now convolve with the beam.
-
-            x, y = numpy.meshgrid(numpy.linspace(-256,255,512), \
-                    numpy.linspace(-256,255,512))
-
-            beam = misc.gaussian2d(x, y, 0., 0., \
-                    visibilities["image"][j].header["BMAJ"]/2.355/\
-                    visibilities["image"][j].header["CDELT2"], \
-                    visibilities["image"][j].header["BMIN"]/2.355/\
-                    visibilities["image"][j].header["CDELT2"], \
-                    (90-visibilities["image"][j].header["BPA"])*\
-                    numpy.pi/180., 1.0)
-
-            for ind in range(len(wave)):
-                m.images[visibilities["lam"][j]].image[:,:,ind,0] = \
-                        scipy.signal.fftconvolve(\
-                        m.images[visibilities["lam"][j]].image[:,:,ind,0], \
-                        beam, mode="same")
-
-            if args.plot_vis:
-                m.visibilities[visibilities["lam"][j]] = \
-                        uv.average(m.visibilities[visibilities["lam"][j]], \
-                        gridsize=20, radial=True, log=True, \
-                        logmin=m.visibilities[visibilities["lam"][j]].uvdist[\
-                        numpy.nonzero(m.visibilities[visibilities["lam"][j]].\
-                        uvdist)].min()*0.95, logmax=m.visibilities[\
-                        visibilities["lam"][j]].uvdist.max()*1.05, \
-                        mode="spectralline")
-
-    os.system("rm params.txt")
-    os.chdir(original_dir)
-    os.system("rmdir /tmp/temp_{1:s}_{0:d}".format(comm.Get_rank(), source))
-
-    return m
-
 # Define a likelihood function.
 
-def lnlike(p, visibilities, parameters, plot):
-    keys = []
-    for key in sorted(parameters.keys()):
-        if not parameters[key]["fixed"]:
-            keys.append(key)
+def lnlike(params, visibilities, parameters, plot):
 
-    params = dict(zip(keys, p))
-
-    # Generate a model.
-
-    m = model(visibilities, params, parameters, plot)
+    m = modeling.run_flared_model(visibilities, params, parameters, plot, \
+            ncpus=ncpus, source=source, plot_vis=args.plot_vis, nice=nice)
 
     # A list to put all of the chisq into.
 
@@ -383,14 +104,7 @@ def lnlike(p, visibilities, parameters, plot):
 
 # Define a prior function.
 
-def lnprior(p, parameters):
-    keys = []
-    for key in sorted(parameters.keys()):
-        if not parameters[key]["fixed"]:
-            keys.append(key)
-
-    params = dict(zip(keys, p))
-
+def lnprior(params, parameters, priors):
     for key in parameters:
         if not parameters[key]["fixed"]:
             if parameters[key]["limits"][0] <= params[key] <= \
@@ -447,18 +161,37 @@ def lnprior(p, parameters):
         else:
             return -numpy.inf
 
-    # Everything was correct, so continue on.
+    # Everything was correct, so continue on and check the priors.
 
-    if (not parameters["dpc"]["fixed"]) and \
-            (parameters["dpc"]["prior"] == "gaussian"):
-        return -0.5 * (params["dpc"] - parameters["dpc"]["value"])**2 / \
-                parameters["dpc"]["sigma"]**2
-    else:
-        return 0.0
+    lnprior = 0.
+
+    # The prior on parallax (distance).
+
+    if (not parameters["dpc"]["fixed"]) and ("parallax" in priors):
+        parallax_mas = 1. / params["dpc"] * 1000
+
+        lnprior += -0.5 * (parallax_mas - priors["parallax"]["value"])**2 / \
+                priors["parallax"]["sigma"]**2
+
+    # A prior on stellar mass from the IMF.
+
+    if (not parameters["logM_star"]["fixed"]) and ("Mstar" in priors):
+        Mstar = 10.**params["logM_star"]
+
+        if priors["Mstar"]["value"] == "chabrier":
+            if Mstar <= 1.:
+                lnprior += numpy.log(0.158 * 1./(numpy.log(10.) * Mstar) * \
+                        numpy.exp(-(numpy.log10(Mstar) - numpy.log10(0.08))**2/\
+                        (2*0.69**2)))
+            else:
+                lnprior += numpy.log(4.43e-2 * Mstar**-1.3 * \
+                        1./(numpy.log(10.) * Mstar))
+
+    return lnprior
 
 # Define a probability function.
 
-def lnprob(p, visibilities, parameters, plot):
+def lnprob(p, visibilities, parameters, priors, plot):
 
     keys = []
     for key in sorted(parameters.keys()):
@@ -467,7 +200,7 @@ def lnprob(p, visibilities, parameters, plot):
 
     params = dict(zip(keys, p))
 
-    lp = lnprior(params, parameters)
+    lp = lnprior(params, parameters, priors)
 
     if not numpy.isfinite(lp):
         return -numpy.inf
@@ -530,6 +263,10 @@ visibilities["data"] = []
 visibilities["data1d"] = []
 visibilities["image"] = []
 
+# Check that all of the parameters are correct.
+
+parameters = modeling.check_parameters(parameters)
+
 # Make sure "fmt" is in the visibilities dictionary.
 
 if not "fmt" in visibilities:
@@ -537,48 +274,18 @@ if not "fmt" in visibilities:
 
 # Decide whether to use an exponentially tapered 
 
-if not "disk_type" in parameters:
-    parameters["disk_type"] = {"fixed":True, "value":"truncated", \
-            "limits":[0.,0.]}
-
 if args.withexptaper:
     parameters["disk_type"]["value"] = "exptaper"
 
-# Make sure the code doesn't break if envelope_type isn't specified.
-
-if not "envelope_type" in parameters:
-    parameters["envelope_type"] = {"fixed":True, "value":"none", \
-            "limits":[0.,0.]}
-
 # Decide whether to do continuum subtraction or not.
-
-if not "docontsub" in parameters:
-    parameters["docontsub"] = {"fixed":True, "value":False, "limits":[0.,0.]}
 
 if args.withcontsub:
     parameters["docontsub"]["value"] = True
 
-# Make sure the code is backwards compatible to a time when only a single gas
-# file was being supplied.
+# Define the priors as an empty dictionary, if no priors were specified.
 
-if "gas_file" in parameters:
-    parameters["gas_file1"] = parameters["gas_file"]
-    parameters["logabundance1"] = parameters["logabundance"]
-
-# Make sure the code doesn't break if dust_type isn't specified.
-
-if not "dust_file" in parameters:
-    parameters["dust_file"] = {"fixed":True, "value":"pollack_new.hdf5", \
-            "limits":[0.,0.]}
-
-# Make sure the code doesn't break if dust extinction parameters aren't included
-
-if not "tau0" in parameters:
-    parameters["tau0"] = {"fixed":True, "value":0., "limits":[0.,10.]}
-if not "v_ext" in parameters:
-    parameters["v_ext"] = {"fixed":True, "value":4., "limits":[2.,6.]}
-if not "sigma_vext" in parameters:
-    parameters["sigma_vext"] = {"fixed":True, "value":1.0, "limits":[0.01,5.]}
+if not 'priors' in globals():
+    priors = {}
 
 ######################################
 # Read in the millimeter visibilities.
@@ -810,7 +517,8 @@ while nsteps < max_nsteps:
 
     # Create a high resolution model for averaging.
 
-    m = model(visibilities, params, parameters, plot=True)
+    m = modeling.run_flared_model(visibilities, params, parameters, plot=True, \
+            ncpus=ncpus, source=source, plot_vis=args.plot_vis, nice=nice)
 
     # Open up a pdf file to plot into.
 
@@ -826,9 +534,14 @@ while nsteps < max_nsteps:
 
         # Calculate the velocity for each image.
 
-        v = c * (float(visibilities["freq"][j])*1.0e9 - \
-                visibilities["image"][j].freq)/(float(visibilities["freq"][j])*\
-                1.0e9)
+        if args.plot_vis:
+            v = c * (float(visibilities["freq"][j])*1.0e9 - \
+                    visibilities["data"][j].freq)/ \
+                    (float(visibilities["freq"][j])*1.0e9)
+        else:
+            v = c * (float(visibilities["freq"][j])*1.0e9 - \
+                    visibilities["image"][j].freq)/ \
+                    (float(visibilities["freq"][j])*1.0e9)
 
         # Plot the image.
 
