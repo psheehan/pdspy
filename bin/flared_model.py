c3f5682dfe85120a024ffc55e906bbd7f9e6743b
Patrick Sheehan <psheehan287@gmail.com>
2019-09-05 21:54:41 -0500

    Improve the names of the parameters going in to the triangle plots.

--- bin/flared_model.py
+++ bin/flared_model.py
@@ -506,7 +506,7 @@ while nsteps < max_nsteps:
     # Plot histograms of the resulting parameters.
 
     labels = ["$"+key.replace("T0_env","T_0,env").replace("T0","T_0").\
-            replace("turb_env","turb,env").\
+            replace("turb_env","turb,env").replace("in_env","in,env").\
             replace("_","_{").replace("log","\log ")+"}$" if key[0:3] == \
             "log" else "$"+key+"$" for key in keys]
 
