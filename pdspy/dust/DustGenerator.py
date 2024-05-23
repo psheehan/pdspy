afef1a18ab12f2ac9d3b6990ab8384dadd781df2
Patrick Sheehan <psheehan287@gmail.com>
2024-05-23 01:41:39 +0000

    Move dust recipes into DustGenerator and remove the original recipes and data files.

--- pdspy/dust/DustGenerator.py
+++ pdspy/dust/DustGenerator.py
@@ -1,72 +1,144 @@
 import numpy
 import scipy
 import h5py
+import os
 from .Dust import Dust
+from .mix_dust import mix_dust
 
 class DustGenerator:
+    recipes = ["draine","pollack","diana","diana_wice"]
+
     def __init__(self, dust, with_dhs=False, fmax=0.8, nf=50, singlesize=False):
         if type(dust) == str:
-            self.read(dust)
-        else:
-            self.rho = dust.rho
+            if dust in self.recipes:
+                recipe_dict = {
+                    "draine":{
+                        "dust":["astronomical_silicates","graphite_parallel_0.01","graphite_perpendicular_0.01"],
+                        "format":["draine","draine","draine"],
+                        "density":numpy.array([3.3,2.24,2.24]),
+                        "abundance":numpy.array([0.65,0.35*1./3,0.35*2./3])
+                    },
+                    "dsharp":{
+                        "dust":["astronomical_silicates","troilite","organics","water_ice"],
+                        "format":["draine","henn","henn","henn"],
+                        "density":numpy.array([3.3,4.83,1.5,0.92]),
+                        "abundance":numpy.array([0.1670,0.0258,0.4430,0.3642])
+                    },
+                    "pollack":{
+                        "dust":["astronomical_silicates","troilite","organics","water_ice"],
+                        "format":["draine","henn","henn","henn"],
+                        "density":numpy.array([3.3,4.83,1.5,0.92]),
+                        "mass_fraction":numpy.array([3.41e-3,7.68e-4,4.13e-3,5.55e-3]),
+                    },
+                    "diana":{
+                        "dust":["amorphous_silicates_extrapolated","amorphous_carbon_zubko1996_extrapolated"],
+                        "format":["henn","henn"],
+                        "density":numpy.array([3.3,1.0]),
+                        "abundance":numpy.array([0.8,0.2]),
+                        "filling":0.75,
+                        "with_dhs":True,
+                    },
+                    "diana_wice":{
+                        "dust":["amorphous_silicates_extrapolated","amorphous_carbon_zubko1996_extrapolated","water_ice"],
+                        "format":["henn","henn","henn"],
+                        "density":numpy.array([3.3,1.0,0.92]),
+                        "abundance":numpy.array([0.8,0.2,0.5]),
+                        "filling":0.75,
+                        "with_dhs":True,
+                    },
+                }
 
-            self.amax = numpy.logspace(-4.,1.,60)
-            self.p = numpy.linspace(2.5,4.5,11)
-            self.lam = dust.lam
+                water_ice = Dust()
+                water_ice.set_optical_constants_from_henn(os.path.dirname(__file__)+"/optical_constants/water_ice.txt")
 
-            self.kabs = []
-            self.ksca = []
-            self.kext = []
-            self.albedo = []
+                species = []
+                for i in range(len(recipe_dict[dust])):
+                    species.append(Dust())
+                    if recipe_dict[dust]["format"][i] == "henn":
+                        species[-1].set_optical_constants_from_henn(os.path.dirname(__file__)+"/optical_constants/"+recipe_dict[dust]["dust"][i]+".txt")
+                        if "extrapolated" in recipe_dict[dust]["dust"][i]:
+                            species[-1].calculate_optical_constants_on_wavelength_grid(water_ice.lam)
+                    elif recipe_dict[dust]["format"][i] == "draine":
+                        species[-1].set_optical_constants_from_draine(os.path.dirname(__file__)+"/optical_constants/graphite_parallel_0.01.txt")
+                        species[-1].calculate_optical_constants_on_wavelength_grid(water_ice.lam)
 
-            a = numpy.logspace(numpy.log10(0.05e-4),1.,500)
-            if singlesize:
-                self.amax = a
-            kabsgrid = numpy.zeros((self.lam.size, a.size))
-            kscagrid = numpy.zeros((self.lam.size, a.size))
+                    species[-1].set_density(recipe_dict[dust]["density"][i])
 
-            for i in range(a.size):
-                if with_dhs:
-                    dust.calculate_dhs_opacity(a[i], fmax=fmax, nf=nf, nang=1)
+                if "mass_fraction" in recipe_dict[dust]:
+                    abundances = (recipe_dict[dust]["mass_fraction"]/recipe_dict[dust]["density"])/(recipe_dict[dust]["mass_fraction"]/recipe_dict[dust]["density"]).sum()
                 else:
-                    dust.calculate_opacity(a[i], coat_volume_fraction=0.0, \
-                            nang=1)
+                    abundances = recipe_dict[dust]["abundance"] / recipe_dict[dust]["abundance"].sum()
 
-                kabsgrid[:,i] = dust.kabs
-                kscagrid[:,i] = dust.ksca
+                if "filling" not in recipe_dict[dust]:
+                    recipe_dict[dust]["filling"] = 1.
 
-            for p in self.p:
-                kabs_temp = []
-                ksca_temp = []
-                kext_temp = []
-                albedo_temp = []
+                if "with_dhs" in recipe_dict[dust]:
+                    with_dhs = recipe_dict[dust]["with_dhs"]
 
-                for amax in self.amax:
-                    if singlesize:
-                        normfunc = numpy.zeros(a.size)
-                        normfunc[a == amax] = 1.
-                    else:
-                        normfunc = a**(3-p)
-                        normfunc[a > amax] = 0.
+                dust = mix_dust(species, abundances, filling=recipe_dict[dust]["filling"])
+            else:
+                self.read(dust)
+                return
+        
+        self.rho = dust.rho
 
-                    norm = scipy.integrate.trapz(normfunc, x=a)
+        self.amax = numpy.logspace(-4.,1.,60)
+        self.p = numpy.linspace(2.5,4.5,11)
+        self.lam = dust.lam
 
-                    kabs_temp.append(scipy.integrate.trapz(kabsgrid*normfunc,\
-                            x=a, axis=1)/norm)
-                    ksca_temp.append(scipy.integrate.trapz(kscagrid*normfunc,\
-                            x=a, axis=1)/norm)
-                    kext_temp.append(kabs_temp[-1] + ksca_temp[-1])
-                    albedo_temp.append( ksca_temp[-1] / kext_temp[-1])
+        self.kabs = []
+        self.ksca = []
+        self.kext = []
+        self.albedo = []
 
-                self.kabs.append(kabs_temp)
-                self.ksca.append(ksca_temp)
-                self.kext.append(kext_temp)
-                self.albedo.append(albedo_temp)
+        a = numpy.logspace(numpy.log10(0.05e-4),1.,500)
+        if singlesize:
+            self.amax = a
+        kabsgrid = numpy.zeros((self.lam.size, a.size))
+        kscagrid = numpy.zeros((self.lam.size, a.size))
 
-            self.kabs = numpy.array(self.kabs)
-            self.ksca = numpy.array(self.ksca)
-            self.kext = numpy.array(self.kext)
-            self.albedo = numpy.array(self.albedo)
+        for i in range(a.size):
+            if with_dhs:
+                dust.calculate_dhs_opacity(a[i], fmax=fmax, nf=nf, nang=1)
+            else:
+                dust.calculate_opacity(a[i], coat_volume_fraction=0.0, \
+                        nang=1)
+
+            kabsgrid[:,i] = dust.kabs
+            kscagrid[:,i] = dust.ksca
+
+        for p in self.p:
+            kabs_temp = []
+            ksca_temp = []
+            kext_temp = []
+            albedo_temp = []
+
+            for amax in self.amax:
+                if singlesize:
+                    normfunc = numpy.zeros(a.size)
+                    normfunc[a == amax] = 1.
+                else:
+                    normfunc = a**(3-p)
+                    normfunc[a > amax] = 0.
+
+                norm = scipy.integrate.trapz(normfunc, x=a)
+
+                kabs_temp.append(scipy.integrate.trapz(kabsgrid*normfunc,\
+                        x=a, axis=1)/norm)
+                ksca_temp.append(scipy.integrate.trapz(kscagrid*normfunc,\
+                        x=a, axis=1)/norm)
+                kext_temp.append(kabs_temp[-1] + ksca_temp[-1])
+                albedo_temp.append( ksca_temp[-1] / kext_temp[-1])
+
+            self.kabs.append(kabs_temp)
+            self.ksca.append(ksca_temp)
+            self.kext.append(kext_temp)
+            self.albedo.append(albedo_temp)
+
+        self.kabs = numpy.array(self.kabs)
+        self.ksca = numpy.array(self.ksca)
+        self.kext = numpy.array(self.kext)
+        self.albedo = numpy.array(self.albedo)
 
     def __call__(self, amax, p=None):
         if self.old:
