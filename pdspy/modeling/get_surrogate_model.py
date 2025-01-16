4aa81166616ea8a50946cc0ef8942c15b7948663
Patrick Sheehan <psheehan287@gmail.com>
2025-01-16 14:24:46 -0600

    Use gpytorch in putting together the surrogate model in the modeling package.

--- pdspy/modeling/get_surrogate_model.py
+++ pdspy/modeling/get_surrogate_model.py
@@ -1,4 +1,11 @@
-import schwimmbad
+import torch
+import gpytorch
+
+from gpytorch.models import ApproximateGP
+from gpytorch.variational import CholeskyVariationalDistribution
+from gpytorch.variational import VariationalStrategy, \
+        IndependentMultitaskVariationalStrategy
+
 import pickle
 import numpy
 import time
@@ -7,73 +14,76 @@ import os
 def get_surrogate_model(params, model="pringle+ulrich+diana", \
         quantity="temperature", nthreads=1):
 
+    fitdir = os.path.dirname(os.path.abspath(__file__))+\
+            "/surrogate_models/{0:s}/{1:s}".format(model, quantity)
+
     # Load the keys for the parameters of the surrogate model.
 
-    keys = list(numpy.loadtxt(os.path.dirname(os.path.abspath(__file__))+\
-            "/surrogate_models/{0:s}/{1:s}/keys.txt".format(model, quantity), \
-            dtype=str))
+    keys = list(numpy.loadtxt(fitdir+"/keys.txt", dtype=str))
 
     # Load in the PCA that was found.
 
-    pca = pickle.load(open(os.path.dirname(os.path.abspath(__file__))+\
-            "/surrogate_models/{0:s}/{1:s}/pca.pkl".format(model, quantity), \
-            "rb"))
+    pca = pickle.load(open(fitdir+"/pca.pkl", "rb"))
 
-    # Also the transformed data
+    # Load in the Gaussian process fits.
 
-    y_grid = numpy.load(os.path.dirname(os.path.abspath(__file__))+\
-            "/surrogate_models/{0:s}/{1:s}/transformed_data.npy".\
-            format(model, quantity))
+    torch.load(fitdir+"/inducing_points.pt")
 
-    # Also load in the Gaussian process fits.
+    model = GPModel(inducing_points=inducing_points)
+    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(\
+            num_tasks=ncomponents, rank=0, has_global_noise=False)
 
-    gps = pickle.load(open(os.path.dirname(os.path.abspath(__file__))+\
-            "/surrogate_models/{0:s}/{1:s}/gps.pkl".format(model, quantity), \
-            "rb"))
-
-    # Load the samples and make sure the best fit values are used for the 
-    # hyperparameters.
-
-    ncomponents = y_grid.shape[1]
-    ncomponents = 9
-
-    samples = []
-    for i in range(ncomponents):
-        samples += [numpy.load(os.path.dirname(os.path.abspath(__file__))+\
-                "/surrogate_models/{0:s}/{1:s}/gp_samples_component{2:d}.pkl."
-                "npy".format(model, quantity, i))]
+    model.load_state_dict(torch.load(fitdir+'/gp.pth'))
+    likelihood.load_state_dict(torch.load(fitdir+'/likelihood.pth'))
 
     # Load in the data.
 
     x = []
-    data = []
-
     x.append([(params[k] - -9.)**3 if k == "logM_env" else params[k] for k in \
             keys])
-
     x = numpy.array(x)
     shape = (99,100,1)
 
-    # Set the parameter vector for the GPs randomly from the posteriors.
-
-    for i in range(ncomponents):
-        w = numpy.random.randint(samples[i].shape[0])
-
-        gps[i].set_parameter_vector(samples[i][w])
-
     # Reconstruct the data from the PCA + GP fit.
 
-    sample = lambda i: gps[i].sample_conditional(y_grid[:,i], x)[0] if \
-            i < ncomponents else 0.
-
-    t1 = time.time()
-    with schwimmbad.JoblibPool(nthreads) as pool:
-        components = [list(pool.map(sample, range(15)))]
-
-    projected = pca.inverse_transform(components)
-    t2 = time.time()
-    print("Time to reconstruct = {0:f} seconds".format(t2 - t1))
+    with torch.no_grad():
+        t1 = time.time()
+        components = [numpy.concatenate((\
+                model(torch.from_numpy(x).float()).sample().detach().\
+                numpy()[0],\
+                numpy.repeat(0., y_grid.shape[1] - ncomponents)))]
+        projected = pca.inverse_transform(components)
+        t2 = time.time()
+        print("Time to reconstruct = {0:f} seconds".format(t2 - t1))
 
     # Return the projected quantity.
 
     return 10.**projected[0].reshape(shape)
+
+class GPModel(ApproximateGP):
+    def __init__(self, inducing_points):
+        variational_distribution = gpytorch.variational.\
+                NaturalVariationalDistribution(inducing_points.size(-2), \
+                batch_shape=torch.Size([ncomponents]))
+        variational_strategy = IndependentMultitaskVariationalStrategy(\
+                VariationalStrategy(self, inducing_points, \
+                variational_distribution, learn_inducing_locations=True), \
+                num_tasks=ncomponents)
+
+        super(GPModel, self).__init__(variational_strategy)
+
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=\
+                torch.Size([ncomponents]))
+
+        base_covar_module = gpytorch.kernels.ScaleKernel(\
+                gpytorch.kernels.MaternKernel(nu=1.5, ard_num_dims=len(keys), \
+                active_dims=[0,1,2,3,4,5,6,7], \
+                batch_shape=torch.Size([ncomponents])), \
+                batch_shape=torch.Size([ncomponents]))
+
+        self.covar_module = base_covar_module
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
